f36d3e187a716aafc4666600660d62da27ece147
ysthakur <2004yst@gmail.com>
2021-01-26 09:29:46 -0500

    Automate txt to md

---
+++ reference_to_md.py
@@ -0,0 +1,12 @@
+md = open('docs/reference.md', 'w+', encoding='utf8')
+
+md.write('cmd  |  stack   |out/*effect\n---|---|---\n')
+
+with open('docs/reference.txt', 'r', encoding='utf8') as txt:
+  txt.readline()
+  for line in txt:
+    if line[0] in '<|\\>':
+      line = '\\' + line
+    space = line.index(' ')
+    line = line[:space] + ' |' + line[space:]
+    md.write(line)
