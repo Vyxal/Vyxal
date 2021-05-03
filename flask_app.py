4e216177176c4771af64500ba4a72c8d575caadb
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-05-03 22:20:46 +1000

    why does this work locally but not remotely?

--- flask_app.py
+++ flask_app.py
@@ -5,12 +5,12 @@ import Vyxal
 app = Flask(__name__)
 CORS(app)
 
-import os, sys
+import os, sys, shutil
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/.."
 sys.path.insert(1, THIS_FOLDER)
 
-os.system("rm -rf sessions")
+shutil.rmtree("sessions", ignore_errors=True)
 os.system("mkdir sessions")
 
 sessions = {}
@@ -36,7 +36,7 @@ def execute():
     if session not in sessions:
       return {"stdout": "", "stderr": "The session was invalid! You may need to reload your tab."}
 
-    os.system(f"rm -rf sessions/{session}")
+    shutil.rmtree(f"sessions/{session}", ignore_errors=True)
     os.mkdir(f"sessions/{session}")
 
     with open(f"sessions/{session}/.stdin", "w", encoding="utf-8") as f:
@@ -80,7 +80,7 @@ def execute():
     with open(f"sessions/{session}/.stdout", "r", encoding="utf-8") as x:
         with open(f"sessions/{session}/.stderr", "r", encoding="utf-8") as y:
             val = {"stdout": x.read(), "stderr": y.read()}
-    os.system(f"rm -rf sessions/{session}")
+    shutil.rmtree(f"sessions/{session}", ignore_errors=True)
     return val
 
 
