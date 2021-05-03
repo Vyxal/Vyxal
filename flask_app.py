aeda517a44dc96fda70cac548d7d9173ba482b2b
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-05-04 08:20:59 +1000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,55 +1,96 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-import multiprocessing
+from flask_cors import CORS 
+import multiprocessing, secrets
+import Vyxal
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'n3vagljfd;lkgern;glkn4erg]po_*&)#M(VNP#UC<P{M@OW#X*()R#M*R<JP(R'
+CORS(app)
 
-@app.route('/', methods=('GET', 'POST'))
+import os, sys, shutil
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/.."
+sys.path.insert(1, THIS_FOLDER)
+
+shutil.rmtree("sessions", ignore_errors=True)
+os.system("mkdir sessions")
+
+sessions = {}
+terminated = set()
+
+@app.route('/', methods=('POST','GET'))
 def index():
-    descriptions = parse_file()
-    if request.method == 'POST':
-        print('starting')
-        flags = request.form['flags']
-        code = request.form['code'].replace("\r", "")
-        input_list = request.form["inputs"]
-        header = request.form["header"].replace("\r", "")
-        footer = request.form["footer"].replace("\r", "")
-        # print(inputs)
-        import Vyxal
-        manager = multiprocessing.Manager()
-        ret = manager.dict()
+    session = secrets.token_hex(64)
+    sessions[session] = None
+    return render_template('main.html', session=session, codepage_info=descriptions)
 
-        if "5" in flags:
-            time = 5
-        elif "T" in flags:
-            time = 10
-        elif "b" in flags:
-            time = 15
-        elif "B" in flags:
-            time = 30
-        else:
-            time = 60
-        ret[1] = ""
-        ret[2] = ""
-        fcode = header + code + footer
-        process = multiprocessing.Process(target=Vyxal.execute, args=(fcode, flags, input_list, ret))
-        process.start()
-        process.join(time)
+@app.route("/execute", methods=('POST',))
+def execute():
 
-        if process.is_alive():
-            process.kill()
-            if 2 in ret:
-                ret[2] += "\n" + f"Code timed out after {time} seconds"
-        # print(ret)
-        output = ret[1]
-        # print(code, flags, output)
-        return render_template('main.html', code=code, header=header, footer=footer, flags=flags, output=output, inputs=input_list, debug=ret[2],
-        codepage_info=descriptions)
+    print(sessions)
+    flags = request.form['flags']
+    code = request.form['code'].replace("\r", "")
+    input_list = request.form["inputs"].replace("\r", "")
+    header = request.form["header"].replace("\r", "")
+    footer = request.form["footer"].replace("\r", "")
+    session = request.form["session"]
 
-    return render_template('main.html', code="", flags="", output="", header="", footer="", inputs="", debug="", codepage_info=descriptions)
+    if session not in sessions:
+      return {"stdout": "", "stderr": "The session was invalid! You may need to reload your tab."}
 
-@app.route("/ash")
-def ash():
-    return render_template("ash.html")
+    shutil.rmtree(f"sessions/{session}", ignore_errors=True)
+    os.mkdir(f"sessions/{session}")
+
+    with open(f"sessions/{session}/.stdin", "w", encoding="utf-8") as f:
+      f.write(input_list)
+    
+    with open(f"sessions/{session}/.stdin", "r", encoding="utf-8") as x:
+      with open(f"sessions/{session}/.stdout", "w", encoding="utf-8") as y:
+        with open(f"sessions/{session}/.stderr", "w", encoding="utf-8") as z:
+            manager = multiprocessing.Manager()
+            ret = manager.dict()
+
+            if "5" in flags:
+                time = 5
+            elif "T" in flags:
+                time = 10
+            elif "b" in flags:
+                time = 15
+            elif "B" in flags:
+                time = 30
+            else:
+                time = 60
+            ret[1] = ""
+            ret[2] = ""
+            fcode = header + code + footer
+            sessions[session] = multiprocessing.Process(target=Vyxal.execute, args=(fcode, flags, input_list, ret))
+            sessions[session].start()
+            sessions[session].join(time)
+            
+            if session in terminated:
+                terminated.remove(session)
+                ret[2] += "\nSession terminated upon user request"
+
+            if sessions[session].is_alive():
+
+                sessions[session].kill()
+                if 2 in ret:
+                    ret[2] += "\n" + f"Code timed out after {time} seconds"
+            output = ret[1]
+            y.write(ret[1])
+            z.write(ret[2])
+    with open(f"sessions/{session}/.stdout", "r", encoding="utf-8") as x:
+        with open(f"sessions/{session}/.stderr", "r", encoding="utf-8") as y:
+            val = {"stdout": x.read(), "stderr": y.read()}
+    shutil.rmtree(f"sessions/{session}", ignore_errors=True)
+    return val
+
+
+@app.route("/kill", methods=("POST",))
+def kill():
+  session = request.form["session"]
+  if sessions.get(session) is None: return ""
+  sessions[session].kill()
+  terminated.add(session)
+  return ""
 
 def parse_file():
     import os
@@ -86,3 +127,5 @@ def parse_file():
                     else:
                         ret.append("\n" + line[1:-1])
     return ret
+
+descriptions = parse_file()
