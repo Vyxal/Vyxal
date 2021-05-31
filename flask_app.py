1042cceed0a23361eb7cda52412c72fb87052d9b
ysthakur <45539777+ysthakur@users.noreply.github.com>
2021-05-31 11:27:37 -0400

    Format flask_app.py with black

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-from flask_cors import CORS 
+from flask_cors import CORS
 import multiprocessing, secrets
 import Vyxal
+
 app = Flask(__name__)
 CORS(app)
 
@@ -16,67 +17,74 @@ os.system("mkdir sessions")
 sessions = {}
 terminated = set()
 
-@app.route('/', methods=('POST','GET'))
+
+@app.route("/", methods=("POST", "GET"))
 def index():
     session = secrets.token_hex(64)
     sessions[session] = None
-    return render_template('main.html', session=session, codepage_info=descriptions)
+    return render_template("main.html", session=session, codepage_info=descriptions)
 
-@app.route("/execute", methods=('POST',))
+
+@app.route("/execute", methods=("POST",))
 def execute():
 
     print(sessions)
-    flags = request.form['flags']
-    code = request.form['code'].replace("\r", "")
+    flags = request.form["flags"]
+    code = request.form["code"].replace("\r", "")
     input_list = request.form["inputs"].replace("\r", "")
     header = request.form["header"].replace("\r", "")
     footer = request.form["footer"].replace("\r", "")
     session = request.form["session"]
 
     if session not in sessions:
-      return {"stdout": "", "stderr": "The session was invalid! You may need to reload your tab."}
+        return {
+            "stdout": "",
+            "stderr": "The session was invalid! You may need to reload your tab.",
+        }
 
     shutil.rmtree(f"sessions/{session}", ignore_errors=True)
     os.mkdir(f"sessions/{session}")
 
     with open(f"sessions/{session}/.stdin", "w", encoding="utf-8") as f:
-      f.write(input_list)
-    
+        f.write(input_list)
+
     with open(f"sessions/{session}/.stdin", "r", encoding="utf-8") as x:
-      with open(f"sessions/{session}/.stdout", "w", encoding="utf-8") as y:
-        with open(f"sessions/{session}/.stderr", "w", encoding="utf-8") as z:
-            manager = multiprocessing.Manager()
-            ret = manager.dict()
+        with open(f"sessions/{session}/.stdout", "w", encoding="utf-8") as y:
+            with open(f"sessions/{session}/.stderr", "w", encoding="utf-8") as z:
+                manager = multiprocessing.Manager()
+                ret = manager.dict()
 
-            if "5" in flags:
-                time = 5
-            elif "T" in flags:
-                time = 10
-            elif "b" in flags:
-                time = 15
-            elif "B" in flags:
-                time = 30
-            else:
-                time = 60
-            ret[1] = ""
-            ret[2] = ""
-            fcode = header + code + footer
-            sessions[session] = multiprocessing.Process(target=Vyxal.execute, args=(fcode, flags, input_list, ret))
-            sessions[session].start()
-            sessions[session].join(time)
-            
-            if session in terminated:
-                terminated.remove(session)
-                ret[2] += "\nSession terminated upon user request"
+                if "5" in flags:
+                    time = 5
+                elif "T" in flags:
+                    time = 10
+                elif "b" in flags:
+                    time = 15
+                elif "B" in flags:
+                    time = 30
+                else:
+                    time = 60
+                ret[1] = ""
+                ret[2] = ""
+                fcode = header + code + footer
+                sessions[session] = multiprocessing.Process(
+                    target=Vyxal.execute, args=(fcode, flags, input_list, ret)
+                )
+                sessions[session].start()
+                sessions[session].join(time)
 
-            if sessions[session].is_alive():
+                if session in terminated:
+                    terminated.remove(session)
+                    ret[2] += "\nSession terminated upon user request"
 
-                sessions[session].kill()
-                if 2 in ret:
-                    ret[2] += "\n" + f"Code timed out after {time} seconds"
-            output = ret[1]
-            y.write(ret[1])
-            z.write(ret[2])
+                if sessions[session].is_alive():
+
+                    sessions[session].kill()
+                    if 2 in ret:
+                        ret[2] += "\n" + f"Code timed out after {time} seconds"
+                output = ret[1]
+                y.write(ret[1])
+                z.write(ret[2])
     with open(f"sessions/{session}/.stdout", "r", encoding="utf-8") as x:
         with open(f"sessions/{session}/.stderr", "r", encoding="utf-8") as y:
             val = {"stdout": x.read(), "stderr": y.read()}
@@ -86,24 +94,29 @@ def execute():
 
 @app.route("/kill", methods=("POST",))
 def kill():
-  session = request.form["session"]
-  if sessions.get(session) is None: return ""
-  sessions[session].kill()
-  terminated.add(session)
-  return ""
+    session = request.form["session"]
+    if sessions.get(session) is None:
+        return ""
+    sessions[session].kill()
+    terminated.add(session)
+    return ""
 
-@app.route("/oeis", methods=("GET", ))
+
+@app.route("/oeis", methods=("GET",))
 def oeis():
-    return render_template('oeis.html')
+    return render_template("oeis.html")
+
+
 def parse_file():
     import os
+
     ret = []
     keys = {}
     THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    file = os.path.join(THIS_FOLDER, 'docs/elements.txt')
+    file = os.path.join(THIS_FOLDER, "docs/elements.txt")
     with open(file, "r", encoding="utf8") as txt:
         for line in txt:
-            if line == "\n": #Finished
+            if line == "\n":  # Finished
                 break
             else:
                 is_digraph = line[0] in "k∆øÞ¨" and line[1] != " "
@@ -111,13 +124,13 @@ def parse_file():
                     letter = line[1]
                 else:
                     letter = line[0]
-                
+
                 if letter in keys and letter != " ":
                     index = keys[letter]
                 else:
                     index = -1
                     keys[letter] = len(ret)
-                
+
                 if line[0] == " ":
                     if is_digraph:
                         print(letter, line)
