28bb987dd067659e105dd341f9c0fee69bb8d943
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-06-23 11:08:37 +1000

    Update VyParse.py

--- VyParse.py
+++ VyParse.py
@@ -1,48 +1,13 @@
 import string
-import sympy
 
 class Structure:
-    NONE = 0
-    IF = 1
-    FOR = 2
-    WHILE = 3
-    FUNCTION = 4
-    LAMBDA = 5
-    STRING = 6
-    NUMBER = 7
-    MAP = 8
-    LIST = 9
-    VAR_GET = 10
-    VAR_SET = 11
-    FUNC_REF = 12
-    COMPRESSED_NUMBER = 13
-    COMPRESSED_STRING = 14
-    CHARACTER = 15
-    DICTIONARY_STRING = 16
-    MONAD_TRANSFORMER = 17
-    DYAD_TRANSFORMER = 18
-    TRIAD_TRANSFORMER = 19
-    FOUR_ARITY_TRANSFORMER = 20
-    VARY_TRANSFORMER = 21
+    NONE = 0; IF = 1; FOR = 2; WHILE = 3; FUNCTION = 4; LAMBDA = 5; STRING = 6; NUMBER = 7; MAP = 8
+    LIST = 9; VAR_GET = 10; VAR_SET = 11; FUNC_REF = 12; COMPRESSED_NUMBER = 13; COMPRESSED_STRING = 14; CHARACTER = 15
+    DICTIONARY_STRING = 16; MONAD_TRANSFORMER = 17; DYAD_TRANSFORMER = 18; TRIAD_TRANSFORMER = 19; FOUR_ARITY_TRANSFORMER = 20; VARY_TRANSFORMER = 21
 
 class Keys:
-    STRING = 1
-    NUMBER = 2
-    IF_TRUE = 3
-    IF_FALSE = 4
-    FOR_VAR = 5
-    FOR_BODY = 6
-    WHILE_COND = 7
-    WHILE_BODY = 8
-    FUNC_NAME = 9
-    FUNC_BODY = 10
-    LAMBDA_BODY = 11
-    LIST_ITEM = 12
-    LIST_ITEMS = 13
-    VAR_NAME = 14
-    LAMBDA_ARGS = 15
-    COM_NUM_VALUE = 16
-    COM_STR_VALUE = 17
+    STRING = 1; NUMBER = 2; IF_TRUE = 3; IF_FALSE = 4; FOR_VAR = 5; FOR_BODY = 6; WHILE_COND = 7; WHILE_BODY = 8; FUNC_NAME = 9
+    FUNC_BODY = 10; LAMBDA_BODY = 11; LIST_ITEM = 12; LIST_ITEMS = 13; VAR_NAME = 14; LAMBDA_ARGS = 15; COM_NUM_VALUE = 16; COM_STR_VALUE = 17
 
 Monadic_Transformers = list("¿μςσφɓƈɗƒɠɦƇƑƘƬƲȤv/\\&")
 Dyadic_Transformers = list("ξψωƁƊƓⱮƝ")
@@ -76,8 +41,8 @@ structure_dictionary = { # (open, close, default_key, starting_active_key, secon
     Structure.LIST: ("⟨", "⟩", Keys.LIST_ITEM, Keys.LIST_ITEM, Keys.LIST_ITEM),
     Structure.FUNC_REF: ("°", ";", Keys.FUNC_NAME, Keys.FUNC_NAME, Keys.FUNC_NAME),
     Structure.NUMBER: ("", "", Keys.NUMBER, Keys.NUMBER, Keys.NUMBER),
-    Structure.VAR_GET: ("←", "", Keys.VAR_NAME, Keys.VAR_NAME, Keys.VAR_NAME),
-    Structure.VAR_SET: ("→", "", Keys.VAR_NAME, Keys.VAR_NAME, Keys.VAR_NAME)
+    Structure.VAR_GET: ("", "", Keys.VAR_NAME, Keys.VAR_NAME, Keys.VAR_NAME),
+    Structure.VAR_SET: ("", "", Keys.VAR_NAME, Keys.VAR_NAME, Keys.VAR_NAME)
 }
 
 OPEN = tuple(structure_dictionary[k][0] for k in structure_dictionary)
@@ -162,7 +127,7 @@ def parse(source: str) -> list[tuple]:
     nest_level = 0 # How far deep we are, as in, are we at the uppermost level of the program?
 
     for character in source:
-        print(character, escaped, structure, tokens, active_key, default_key)
+        # print(character, nest_level, escaped, structure, tokens, active_key, default_key)
 
         # First, we do all the conditions that require immediant moving onto the next character (hence the continue)
         if escaped:
@@ -193,34 +158,20 @@ def parse(source: str) -> list[tuple]:
             tokens.append((Structure.STRING, character))
             continue
 
-        # Now we move onto the possibility that we are starting a new kind of token
-        if character in (string.digits + "."):
-            structure = Structure.NUMBER
-            active_key = structure_dictionary[Structure.NUMBER][2]
-            structure_data[active_key] = character
-        
-        elif character == structure_dictionary[Structure.VAR_GET][0]:
-            structure = Structure.VAR_GET
-            active_key = structure_dictionary[Structure.VAR_GET][2]
-            structure_data[active_key] = ""
-        
-        elif character == structure_dictionary[Structure.VAR_SET][0]:
-            structure = Structure.VAR_SET
-            active_key = structure_dictionary[Structure.VAR_SET][2]
-            structure_data[active_key] = ""
-        
-        elif character in OPEN:
+        # Now we move onto the possibility that we are starting a new kind of token        
+        if character in OPEN:
             # Opening character. Note that this won't be variable stuff because I just handeled it. kekw very cool kanye. very cool.
             # no I'm not stalling with comments because I don't want to write the logic here. what gives you that impression?
             if nest_level:
                 nest_level += 1
-                structure_data[active_key] += 1
+                structure_data[active_key] += character
                 continue # That is, we're already in a structure, so we go deeper m'dude.
 
             structure = tuple(k for k in structure_dictionary if character == structure_dictionary[k][0])[0] # there's guaranteed to only be 1, because we have determined that it is in the dictionary
             default_key = structure_dictionary[structure][2]
             active_key = default_key 
             structure_data[active_key] = ""
+            nest_level += 1
 
             # We have to special case lists though
             if structure == Structure.LIST:
@@ -262,6 +213,21 @@ def parse(source: str) -> list[tuple]:
             structure_data[active_key] += character
         elif character in Monadic_Transformers:
             tokens.append((Structure.MONAD_TRANSFORMER, (character, (tokens.pop(),))))
+        
+        elif character in (string.digits + "."):
+            structure = Structure.NUMBER
+            active_key = structure_dictionary[Structure.NUMBER][2]
+            structure_data[active_key] = character
+        
+        elif character == "←":
+            structure = Structure.VAR_GET
+            active_key = structure_dictionary[Structure.VAR_GET][2]
+            structure_data[active_key] = ""
+        
+        elif character == "→":
+            structure = Structure.VAR_SET
+            active_key = structure_dictionary[Structure.VAR_SET][2]
+            structure_data[active_key] = ""
         
         elif character in Dyadic_Transformers:
             elements = (tokens.pop(), tokens.pop())[::-1]
@@ -315,4 +281,3 @@ if __name__ == "__main__":
 
     for test in tests:
         print(test, parse(test))
-
