9c40fc579df8b94d97309f5d017d4d111ef8a2c6
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-08-07 23:08:08 +1000

    Added the basics

--- vyxal/vyxal.py
+++ vyxal/vyxal.py
@@ -15,7 +15,7 @@ def lambda_wrap(branch: list[structure.Structure]) -> structure.Lambda:
     Turns a list of structures into a single lambda structure. Useful
     for dealing with the functions of modifiers. Note that single
     elements pass their arity on to the lambda
-    
+
     Para
     """
 
@@ -33,11 +33,19 @@ def lambda_wrap(branch: list[structure.Structure]) -> structure.Lambda:
     else:
         return structure.Lambda(branch)
 
+
 def transpile(program: str) -> str:
-    """
-    Cool
-    stuff
-    here
-    """
-    
+    program = parse.parse(lexer.tokenise(program))
+    for structure in program:
+        print(structure)
     return ""
+
+
+if __name__ == "__main__":
+    # I'm allowed to have this here this time. Frick you if you say I
+    # can't.
+
+    while True:
+        # Vyxal REPL ftw
+        line = input("   ")
+        line = transpile(line)
