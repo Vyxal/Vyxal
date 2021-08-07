c469847e5a17d4af4dc3c9a92ca0ae46aec8919e
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-08-07 23:16:37 +1000

    There we go, this is a good start

--- vyxal/elements.py
+++ vyxal/elements.py
@@ -6,4 +6,35 @@ the python equivalent of command is stored
 """
 
 from helpers import *
+
 # or should that be import helpers?
+
+
+def process_element(expr: str, arity: int) -> str:
+    """
+    Takes a python expression and adds boilerplate to it corresponding
+    to the arity of the element being processed.
+
+    Parameters
+    ----------
+
+    expr : str
+        The expression to wrap in boilerplate
+
+    arity : int
+        The arity of the element
+
+    Returns
+    -------
+
+    str
+        See documents/specs/Transpilation.md for information
+    """
+
+    return "Not Implemented Yet"
+
+
+elements = {
+    # having a dictionary with the same name as the library can't go
+    # wrong now can it? \s
+}
