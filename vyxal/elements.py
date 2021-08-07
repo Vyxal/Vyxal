8a707772fb7131a7be8682a1749962b16b18058d
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-08-07 23:12:39 +1000

    create 2 really cool files

---
+++ vyxal/elements.py
@@ -0,0 +1,9 @@
+"""
+File: elements.py
+Description: This is where the element functions are stored (that is, 
+functions directly corresponding to Vyxal elements). It's also where
+the python equivalent of command is stored
+"""
+
+from helpers import *
+# or should that be import helpers?
---
+++ vyxal/helpers.py
@@ -0,0 +1,6 @@
+"""
+File: helpers.py
+Description: This is where the cool functions go that help out stuff.
+They aren't directly attached to an element. Consequently, you need to
+use type annotations here.
+"""
