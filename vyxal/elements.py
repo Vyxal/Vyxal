778a86c4d6c6b4506698670a9590ea32d7b7d3b1
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-08-11 18:54:52 +1000

    add ability to turn functions into elements
    - also, handle nilads correctly and add a few example elements

--- vyxal/elements.py
+++ vyxal/elements.py
@@ -4,13 +4,20 @@ Description: This is where the element functions are stored (that is,
 functions directly corresponding to Vyxal elements). It's also where
 the python equivalent of command is stored
 """
-
+import types
+from typing import Union
 from helpers import *
 
 # or should that be import helpers?
 
 
-def process_element(expr: str, arity: int) -> str:
+def add(lhs, rhs, ctx):
+    return lhs + rhs
+
+
+def process_element(
+    expr: Union[str, types.FunctionType], arity: int
+) -> tuple[str, int]:
     """
     Takes a python expression and adds boilerplate to it corresponding
     to the arity of the element being processed.
@@ -31,12 +38,25 @@ def process_element(expr: str, arity: int) -> str:
         See documents/specs/Transpilation.md for information
     """
 
-    arguments = ", ".join(["third", "rhs", "lhs"][-arity:])
+    arguments = ["third", "rhs", "lhs"][-arity:] if arity else "_"
+    if isinstance(expr, types.FunctionType):
+        return (
+            f"{', '.join(arguments)} = pop(stack, {arity}, ctx); "
+            f"stack.append({expr.__name__}({', '.join(arguments[::-1])}, ctx))",
+            arity,
+        )
+    else:
+        return (
+            f"{', '.join(arguments)} = pop(stack, {arity}, ctx); "
+            f"stack.append({expr})",
+            arity,
+        )
 
-    return (
-        f"{arguments} = pop(stack, {arity}, ctx); stack.append({expr})",
-        arity,
-    )
 
+elements = {
+    "+": process_element(add, 2),
+    "!": process_element("len(stack)", 0),
+    ":": "lhs = pop(stack, ctx=ctx); stack.append(copy(lhs)); stack.append(copy(lhs))",
+}
 
-elements = {"+": process_element("add(lhs, rhs, ctx)", 2)}
+print(elements)
