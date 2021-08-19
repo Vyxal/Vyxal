e476cf2cb2cf48d2190518648d637d5cda4ada6d
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-08-19 23:37:14 +1000

    It's way too late to do vectorisation properly

--- vyxal/elements.py
+++ vyxal/elements.py
@@ -3,8 +3,16 @@
 (that is, functions directly corresponding to Vyxal elements). It's also where
 the python equivalent of command is stored
 """
+
+import math
 import types
 from typing import Union
+
+import sympy
+from vyxal import helpers
+from vyxal import LazyList
+
+NUMBER_TYPE = "number"
 
 
 def process_element(
@@ -33,11 +41,49 @@ def process_element(
     return py_code, arity
 
 
+def log_mold_multi(lhs, rhs, ctx):
+    """Element •
+    (num, num) -> log_lhs(rhs)
+    (num, str) -> [char * lhs for char in rhs]
+    (str, num) -> [char * rhs for char in lhs]
+    (str, str) -> lhs.with_capitalisation_of(rhs)
+    (lst, lst) -> lhs molded to the shape of rhs
+    """
+
+    ts = vy_type(lhs, rhs, True)
+
+    return {
+        (NUMBER_TYPE, NUMBER_TYPE): lambda: sympy.Rational(math.log(lhs, rhs)),
+        (NUMBER_TYPE, str): lambda: "".join([char * lhs for char in rhs]),
+        (str, NUMBER_TYPE): lambda: "".join([char * rhs for char in lhs]),
+        (str, str): lambda: helpers.transfer_capitalisation(rhs, lhs),
+        (list, list): lambda: helpers.mold(lhs, rhs),
+    }.get(ts, lambda: vectorise(log_mold_multi, lhs, rhs, ctx=ctx))()
+
+
+def vectorise(function, lhs, rhs=None, other=None, ctx=None):
+    """Do that thing with the things."""
+
+
+def vy_type(item, other=None, simple=False):
+    if other is not None:
+        return (vy_type(item, simple=simple), vy_type(other, simple=simple))
+    if (x := type(item)) in (int, sympy.Rational, complex):
+        return NUMBER_TYPE
+    elif simple and isinstance(item, LazyList.LazyList):
+        return list
+    else:
+        return x
+
+
 elements: dict[str, tuple[str, int]] = {
     "¬": process_element("int(not lhs)", 1),
     "∧": process_element("int(lhs and rhs)", 2),
     "⟑": process_element("int(rhs and lhs)", 2),
     "∨": process_element("int(lhs or rhs)", 2),
     "⟇": process_element("int(rhs or lhs)", 2),
+    "÷": ("lhs = pop(stack, 1, ctx); stack += iterable(lhs)", 1),
+    "×": process_element("'*'", 0),
+    "•": process_element(log_mold_multi, 2),
 }
 modifiers = {}
--- vyxal/helpers.py
+++ vyxal/helpers.py
@@ -8,6 +8,7 @@ import textwrap
 from typing import Union
 
 from vyxal import lexer
+from vyxal import LazyList
 
 
 def indent_str(string: str, indent: int, end="\n") -> str:
@@ -19,6 +20,40 @@ def indent_str(string: str, indent: int, end="\n") -> str:
 def indent_code(*code, indent: int = 1) -> str:
     """Indent multiple lines (`*code`) by the given amount, then join on newlines."""
     return "\n".join(indent_str(line, indent, end="") for line in code) + "\n"
+
+
+def mold(
+    content: Union[list, LazyList.LazyList],
+    shape: Union[list, LazyList.LazyList],
+) -> Union[list, LazyList.LazyList]:
+    """Mold one list to the shape of the other. Uses the mold function
+    that Jelly uses."""
+    # https://github.com/DennisMitchell/jellylanguage/blob/70c9fd93ab009c05dc396f8cc091f72b212fb188/jelly/interpreter.py#L578
+    for index in range(len(shape)):
+        if type(shape[index]) == list:
+            mold(content, shape[index])
+        else:
+            item = content.pop(0)
+            shape[index] = item
+            content.append(item)
+    return shape
+
+
+def transfer_capitalisation(source: str, target: str) -> str:
+    """Returns target with the capitalisation of source"""
+    ret = ""
+    for i in range(min(len(source), len(target))):
+        if source[i].isupper():
+            ret += target[i].upper()
+        elif source[i].islower():
+            ret += target[i].lower()
+        else:
+            ret += target[i]
+
+    if len(target) > len(source):
+        ret += target[i + 1 :]
+
+    return ret
 
 
 def uncompress(token: lexer.Token) -> Union[int, str]:
