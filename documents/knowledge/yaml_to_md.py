df2ca5c8564b1b5c405bafbf1241aa80418fb08e
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-11-10 18:13:03 +1100

    Hmm yes too big

--- documents/knowledge/yaml_to_md.py
+++ documents/knowledge/yaml_to_md.py
@@ -10,34 +10,33 @@ with open(ELEMENTS_YAML, "r", encoding="utf-8") as elements:
 
 with open(ELEMENTS_TXT, "w", encoding="utf-8") as output:
     for element in data:
-        for element in data:
-            if "modifier" in element:
-                output.write(
-                    "## " + element["modifier"] + " (" + element["name"] + ")\n"
-                )
-                output.write(element["description"] + "\n\n")
-                output.write("Usage:\n```\n" + element["usage"] + "\n```\n\n")
+        if "modifier" in element:
+            output.write(
+                "## " + element["modifier"] + " (" + element["name"] + ")\n"
+            )
+            output.write(element["description"] + "\n\n")
+            output.write("Usage:\n```\n" + element["usage"] + "\n```\n\n")
 
-            else:
-                output.write(
-                    str("## " + element["element"])
-                    + " ("
-                    + str(element["name"])
-                    + ")\n\n"
-                )
-                output.write(str(element["description"]) + "\n\n")
-                if "overloads" in element:
-                    output.write("### Overloads\n\n")
-                    for overload in element["overloads"]:
-                        data_types = map(
-                            lambda x: " ".join(x),
-                            zip(overload.split("-"), "abc"),
-                        )
-                        output.write(
-                            "- "
-                            + ", ".join(data_types)
-                            + ": `"
-                            + str(element["overloads"][overload])
-                            + "`\n"
-                        )
+        else:
+            output.write(
+                str("## " + element["element"])
+                + " ("
+                + str(element["name"])
+                + ")\n\n"
+            )
+            output.write(str(element["description"]) + "\n\n")
+            if "overloads" in element:
+                output.write("### Overloads\n\n")
+                for overload in element["overloads"]:
+                    data_types = map(
+                        lambda x: " ".join(x),
+                        zip(overload.split("-"), "abc"),
+                    )
+                    output.write(
+                        "- "
+                        + ", ".join(data_types)
+                        + ": `"
+                        + str(element["overloads"][overload])
+                        + "`\n"
+                    )
         output.write("-------------------------------\n")
