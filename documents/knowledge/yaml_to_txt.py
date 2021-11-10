ed1fe48854638b132de20684c1b4fbac491444c5
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-11-10 15:12:36 +1100

    when the elements txt is auto generated

---
+++ documents/knowledge/yaml_to_txt.py
@@ -0,0 +1,35 @@
+import os
+import yaml
+
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+ELEMENTS_YAML = os.path.join(CURR_DIR, "elements.yaml")
+ELEMENTS_TXT = os.path.join(CURR_DIR, "elements.txt")
+
+with open(ELEMENTS_YAML, "r", encoding="utf-8") as elements:
+    data = yaml.safe_load(elements)
+
+with open(ELEMENTS_TXT, "w", encoding="utf-8") as output:
+    for element in data:
+        if "modifier" in element:
+            output.write(element["modifier"] + " (" + element["name"] + ")\n\n")
+            output.write("- " + element["description"] + "\n\n")
+            output.write("    " + element["usage"] + "\n\n")
+
+        else:
+            output.write(
+                str(element["element"]) + " (" + str(element["name"]) + ")\n\n"
+            )
+            output.write("- " + str(element["description"]) + "\n\n")
+            if "overloads" in element:
+                for overload in element["overloads"]:
+                    data_types = map(
+                        lambda x: " ".join(x), zip(overload.split("-"), "abc")
+                    )
+                    output.write(
+                        "    "
+                        + ", ".join(data_types)
+                        + ": "
+                        + str(element["overloads"][overload])
+                        + "\n"
+                    )
+        output.write("-------------------------------\n")
