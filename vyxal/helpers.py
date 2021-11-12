c94233e328577cac5c63ebd069fc1075000b5fcd
Razetime <rraghu.11502@gmail.com>
2021-11-12 08:38:15 +0530

    fix ∆I lazylist suggestion

--- vyxal/helpers.py
+++ vyxal/helpers.py
@@ -657,13 +657,18 @@ def wrapify(item: Any, count: int = None, ctx: Context = None) -> List[Any]:
 
 def pi_digits(x: int):
     """Generate x digits of Pi. Spigot's formula."""
-    k,a,b,a1,b1 = 2,4,1,12,4
-    while x > 0:
-        p,q,k = k * k, 2 * k + 1, k + 1
-        a,b,a1,b1 = a1, b1, p*a + q*a1, p*b + q*b1
-        d,d1 = a/b, a1/b1
-        while d == d1 and x > 0:
-            yield int(d)
-            x -= 1
-            a,a1 = 10*(a % b), 10*(a1 % b1)
+    
+    @lazylist
+    def gen():
+        k,a,b,a1,b1 = 2,4,1,12,4
+        while x > 0:
+            p,q,k = k * k, 2 * k + 1, k + 1
+            a,b,a1,b1 = a1, b1, p*a + q*a1, p*b + q*b1
             d,d1 = a/b, a1/b1
+            while d == d1 and x > 0:
+                yield int(d)
+                x -= 1
+                a,a1 = 10*(a % b), 10*(a1 % b1)
+                d,d1 = a/b, a1/b1
+
+    return gen()
