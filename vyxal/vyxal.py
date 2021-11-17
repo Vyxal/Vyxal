8c3fdbc6b9746fd2cf14a539aa2119efb675da32
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-11-17 23:06:49 +1100

    started on a CLI

--- vyxal/vyxal.py
+++ vyxal/vyxal.py
@@ -5,8 +5,7 @@ offline.
 
 import types
 
-from argparse import ArgumentParser
-
+import sys
 from vyxal.context import Context
 from vyxal.elements import *
 from vyxal.transpile import transpile
@@ -25,26 +24,41 @@ if __name__ == "__main__":
     ctx = Context()
     stack = []
 
-    # This is called if a file isn't given, just like it used to.
-    ctx.repl_mode = True
-    while True:
-        # Vyxal REPL ftw
-        line = transpile(input(">>> "))
-        stack = []
-        ctx.stacks.append(stack)  # Finally, a use case for assignment by
-        # reference. Never thought I'd fine a time
-        # when it wouldn't be an actual pain.
-        print(line)
-        exec(line)
+    if len(sys.argv) > 1:
+        with open(sys.argv[1], "r", encoding="utf-8") as f:
+            code = f.read()  # TODO: Allow for vyxal raw encoding.
+            # That'll be done once flags are handled
 
-        res = []
-        while len(stack):
-            top = stack.pop()
-            if isinstance(top, types.FunctionType):
-                res.append(top(stack, top, ctx=ctx)[-1])
-            else:
-                res.append(top)
-        res = res[::-1]
+        if len(sys.argv) > 2:
+            flags = sys.argv[2]
+            if len(sys.argv) > 3:
+                ctx.inputs = list(map(lambda x: vy_eval(x, ctx), sys.argv[3:]))
 
-        vy_print(res, ctx=ctx)
-        ctx.stacks.pop()
+        code = transpile(code)
+        ctx.stacks.append(stack)
+        exec(code)
+        print(stack[-1])
+    else:
+        # This is called if a file isn't given, just like it used to.
+        ctx.repl_mode = True
+        while True:
+            # Vyxal REPL ftw
+            line = transpile(input(">>> "))
+            stack = []
+            ctx.stacks.append(stack)  # Finally, a use case for assignment by
+            # reference. Never thought I'd fine a time
+            # when it wouldn't be an actual pain.
+            print(line)
+            exec(line)
+
+            res = []
+            while len(stack):
+                top = stack.pop()
+                if isinstance(top, types.FunctionType):
+                    res.append(top(stack, top, ctx=ctx)[-1])
+                else:
+                    res.append(top)
+            res = res[::-1]
+
+            vy_print(res, ctx=ctx)
+            ctx.stacks.pop()
