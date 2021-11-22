78f5178b4ca1347ab032eba374d59cd67e9dd11b
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-11-22 18:22:49 +1100

    I'mma gon retry making this a package

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="vyxal",
-    version="2.6.0p2",
-    packages=["vyxal"],
-    scripts=["scripts/vyxal"],
-    install_requires=["sympy", "num2words", "numpy"],
-)
