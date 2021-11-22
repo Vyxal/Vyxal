18372c811a89fbe7d7aa1a06cefcb95819942fee
cgccuser <0foobarbaz@gmail.com>
2021-11-22 18:33:24 -0500

    Use 'rc' for the version suffix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="vyxal",
-    version="2.6.0pre2",
+    version="2.6.0rc2",
     description="The Vyxal Programming Language",
     url="https://github.com/Vyxal/Vyxal",
     author="Vyxal Organisation",
