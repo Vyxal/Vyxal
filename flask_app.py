174f55615b83f81ded62d11fcadd95098346c419
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-11-23 18:12:47 +1100

    ah yes flask app.

---
+++ flask_app.py
@@ -0,0 +1,143 @@
+import multiprocessing
+import secrets
+
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+import vyxal
+from vyxal.context import Context
+
+app = Flask(__name__)
+CORS(app)
+
+import os
+import shutil
+import sys
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/.."
+sys.path.insert(1, THIS_FOLDER)
+
+shutil.rmtree("sessions", ignore_errors=True)
+os.system("mkdir sessions")
+
+sessions = {}
+terminated = set()
+
+
+@app.route("/", methods=("POST", "GET"))
+def index():
+    session = secrets.token_hex(64)
+    sessions[session] = None
+    return render_template("index.html", session=session, codepage_info="No.")
+
+
+@app.route("/execute", methods=("POST",))
+def execute():
+
+    print(sessions, request.form)
+    flags = request.form["flags"]
+    code = request.form["code"].replace("\r", "")
+    input_list = request.form["inputs"].replace("\r", "")
+    header = request.form["header"].replace("\r", "")
+    footer = request.form["footer"].replace("\r", "")
+    session = request.form["session"]
+
+    print(code)
+
+    if session not in sessions:
+        return {
+            "stdout": "",
+            "stderr": "The session was invalid! You may need to reload your tab.",
+        }
+
+    shutil.rmtree(f"sessions/{session}", ignore_errors=True)
+    os.mkdir(f"sessions/{session}")
+
+    with open(f"sessions/{session}/.stdin", "w", encoding="utf-8") as f:
+        f.write(input_list)
+
+    with open(f"sessions/{session}/.stdin", "r", encoding="utf-8") as x:
+        with open(f"sessions/{session}/.stdout", "w", encoding="utf-8") as y:
+            with open(
+                f"sessions/{session}/.stderr", "w", encoding="utf-8"
+            ) as z:
+                manager = multiprocessing.Manager()
+                ret = manager.dict()
+
+                if "5" in flags:
+                    time = 5
+                elif "T" in flags:
+                    time = 60
+                elif "b" in flags:
+                    time = 15
+                elif "B" in flags:
+                    time = 30
+                else:
+                    time = 10
+                ret[1] = ""
+                ret[2] = ""
+                fcode = (
+                    (header and (header + "\n"))
+                    + code
+                    + (footer and ("\n" + footer))
+                )
+
+                ctx = Context()
+                ctx.online = True
+                sessions[session] = multiprocessing.Process(
+                    target=vyxal.execute_vyxal,
+                    args=(fcode, flags, input_list, ret, ctx),
+                )
+                sessions[session].start()
+                sessions[session].join(time)
+
+                if session in terminated:
+                    terminated.remove(session)
+                    ret[2] += "\nSession terminated upon user request"
+
+                if sessions[session].is_alive():
+
+                    sessions[session].kill()
+                    if 2 in ret:
+                        ret[2] += "\n" + f"Code timed out after {time} seconds"
+                print(len(ret[1]))
+                y.write(ret[1])
+                z.write(ret[2])
+    with open(f"sessions/{session}/.stdout", "r", encoding="utf-8") as x:
+        with open(f"sessions/{session}/.stderr", "r", encoding="utf-8") as y:
+            val = {"stdout": x.read(), "stderr": y.read()}
+    shutil.rmtree(f"sessions/{session}", ignore_errors=True)
+    return val
+
+
+@app.route("/kill", methods=("POST",))
+def kill():
+    session = request.form["session"]
+    if sessions.get(session) is None:
+        return ""
+    sessions[session].kill()
+    terminated.add(session)
+    return ""
+
+
+@app.route("/oeis", methods=("GET",))
+def oeis():
+    return render_template("oeis.html")
+
+
+@app.route("/update", methods=("GET", "POST"))
+def update():
+    # Updates the server after a commit
+    # It's possible that it is now working.
+    if request.method == "POST":
+        repo = git.Repo("/home/Lyxal/mysite")
+        origin = repo.remotes.origin
+        with repo.config_writer() as git_config:
+            git_config.set_value(
+                "user", "email", "36217120+Lyxal@users.noreply.github.com"
+            )
+            git_config.set_value("user", "name", "Lyxal")
+        origin.pull()
+        return "Updated PythonAnywhere successfully", 200
+    else:
+        return "Wrong event type", 400
--- vyxal/transpile.py
+++ vyxal/transpile.py
@@ -94,9 +94,7 @@ def transpile_token(
             )
         return indent_str(f"stack.append({token.value})", indent)
     elif token.name == TokenType.GENERAL:
-        return indent_str(
-            elements.elements.get(token.value, ("pass\n", -1))[0], indent
-        )
+        return indent_str(elements.get(token.value, ("pass\n", -1))[0], indent)
     elif token.name == TokenType.COMPRESSED_NUMBER:
         return indent_str(f"stack.append({uncompress(token)})", indent)
     elif token.name == TokenType.COMPRESSED_STRING:
