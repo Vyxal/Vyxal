70214a6b28e0319e8b81ce9b442066724636e6a3
pxeger <_@pxeger.com>
2021-12-23 14:27:39 +1100

    add command-line script

--- vyxal/__main__.py
+++ vyxal/__main__.py
@@ -1,15 +1,3 @@
-import sys
+from vyxal.main import cli
 
-import vyxal.main
-
-if len(sys.argv) > 1:
-    file_name = sys.argv[1]
-    flags = ""
-    inputs = []
-
-    if len(sys.argv) > 2:
-        flags, inputs = sys.argv[2], sys.argv[3:]
-
-    vyxal.main.execute_vyxal(file_name, flags, inputs)
-else:
-    vyxal.main.repl()
+cli()
--- vyxal/main.py
+++ vyxal/main.py
@@ -231,3 +231,17 @@ def repl():
 
         vy_print(res, ctx=ctx)
         ctx.stacks.pop()
+
+
+def cli():
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+        flags = ""
+        inputs = []
+
+        if len(sys.argv) > 2:
+            flags, inputs = sys.argv[2], sys.argv[3:]
+
+        execute_vyxal(file_name, flags, inputs)
+    else:
+        repl()
