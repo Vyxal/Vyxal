101dfca71a792aafac11777df08fabe8a97275c0
Aaron Miller <57011988+AMiller42@users.noreply.github.com>
2022-01-25 10:07:42 -0500

    test gooder negative binery
    that was a typo, but screw it, i aint fixing it

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -4408,6 +4408,27 @@ def test_Binary():
     else:
         assert equals(actual, expected, ctx) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
 
+        
+    stack = [vyxalify(item) for item in [-10]]
+    expected = vyxalify([-1, 0, -1, 0])
+    ctx = Context()
+
+    ctx.stacks.append(stack)
+
+    code = transpile('b')
+    # print('b', code)
+    exec(code)
+
+    ctx.stacks.pop()
+    actual = vyxalify(stack[-1])
+
+    print(simplify(expected), simplify(actual))
+
+    if vy_type(actual, simple=True) is list or vy_type(expected, simple=True) is list:
+        assert all(deep_flatten(equals(actual, expected, ctx), ctx)) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
+    else:
+        assert equals(actual, expected, ctx) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
+        
 
 def test_Contains():
 
