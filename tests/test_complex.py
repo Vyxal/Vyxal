9b0a63e655592b37b192324668cd36e7f9478082
Aaron Miller <57011988+AMiller42@users.noreply.github.com>
2022-04-14 14:57:03 -0400

    Add tests

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -440,3 +440,9 @@ def test_all_multiples():
         "KromerKromerKromerKromerKromerKromerKromerKromerKromer",
         "KromerKromerKromerKromerKromerKromerKromerKromerKromerKromer",
     ]
+    
+def test_empty_lists():
+    stack = run_vyxal("69 ⟨1|2|3|||6⟩")
+    assert stack[-1] == [1, 2, 3, 6]
+    stack = run_vyxal("69 ⟨⟩")
+    assert stack[-1] == []
