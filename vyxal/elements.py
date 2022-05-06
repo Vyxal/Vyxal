1e8c7c1865a7121148a2c901554aa6a7ac37b403
Lyxal <36217120+Lyxal@users.noreply.github.com>
2022-05-06 23:04:33 +1000

    Make τ work with negative bases
    Fixes #920

--- vyxal/elements.py
+++ vyxal/elements.py
@@ -4272,7 +4272,7 @@ def to_base(lhs, rhs, ctx):
         return vectorise(to_base, lhs, rhs, ctx=ctx)
 
     if vy_type(rhs) == NUMBER_TYPE:
-        rhs = list(range(0, int(rhs)))
+        return to_base_digits(lhs, rhs)
     else:
         rhs = iterable(rhs, ctx=ctx)
     if len(rhs) == 1:
