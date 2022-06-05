fd24d7d53b2dd4f5ae0676347bca86b2986f3bd6
Lyxal <36217120+Lyxal@users.noreply.github.com>
2022-06-05 22:51:00 +1000

    Update vyxal/elements.py
    Co-authored-by: cgccuser <91574512+cgccuser@users.noreply.github.com>

--- vyxal/elements.py
+++ vyxal/elements.py
@@ -1739,7 +1739,7 @@ def gen_from_fn(lhs, rhs, ctx):
     def gen():
         yield from lhs
 
-        made = [*lhs]
+        made = lhs[:]
 
         func_arity = (
             rhs.stored_arity if "stored_arity" in dir(rhs) else rhs.arity
