d186933efc7fa25f3210235ad3dfb76621d12b60
Caleb Miller <caleb@mathmammoth.com>
2022-06-08 12:00:50 -0600

    Add element to fill matrix to make it rectangular

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -22060,6 +22060,29 @@ def test_MultidimensionalSearch():
         assert equals(actual, expected, ctx) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
 
 
+def test_Filltomakerectangular():
+
+    stack = [vyxalify(item) for item in [[[1,2,3],[4,5],[6]], 0]]
+    expected = vyxalify([[1, 2, 3], [4, 5, 0], [6, 0, 0]])
+    ctx = Context()
+
+    ctx.stacks.append(stack)
+
+    code = transpile('ÞḞ')
+    # print('ÞḞ', code)
+    exec(code)
+
+    ctx.stacks.pop()
+    actual = vyxalify(stack[-1])
+
+    print(simplify(expected), simplify(actual))
+
+    if vy_type(actual, simple=True) is list or vy_type(expected, simple=True) is list:
+        assert all(deep_flatten(equals(actual, expected, ctx), ctx)) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
+    else:
+        assert equals(actual, expected, ctx) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
+
+
 def test_ZeroMatrix():
 
     stack = [vyxalify(item) for item in [[3,4]]]
