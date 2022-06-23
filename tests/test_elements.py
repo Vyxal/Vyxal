4bb48819d1daf5ae0c24a75e8c8cb7386834b837
lyxal <36217120+Lyxal@users.noreply.github.com>
2022-06-23 22:21:34 +1000

    Make transpose return a list of strings given strings
    Closes #1124
    
    Some of the tests were changed from the tests PR because a singleton is a single column of itself, not part of a row.

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -16661,7 +16661,7 @@ def test_Transpose():
 
 
     stack = [vyxalify(item) for item in [[1,2,3,4]]]
-    expected = vyxalify([[1], [2], [3], [4]])
+    expected = vyxalify([[1, 2, 3, 4]])
     ctx = Context()
 
     ctx.stacks.append(stack)
@@ -16682,7 +16682,49 @@ def test_Transpose():
 
 
     stack = [vyxalify(item) for item in [1234]]
-    expected = vyxalify([[1], [2], [3], [4]])
+    expected = vyxalify([[1, 2, 3, 4]])
+    ctx = Context()
+
+    ctx.stacks.append(stack)
+
+    code = transpile('∩')
+    # print('∩', code)
+    exec(code)
+
+    ctx.stacks.pop()
+    actual = vyxalify(stack[-1])
+
+    print(simplify(expected), simplify(actual))
+
+    if vy_type(actual, simple=True) is list or vy_type(expected, simple=True) is list:
+        assert all(deep_flatten(equals(actual, expected, ctx), ctx)) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
+    else:
+        assert equals(actual, expected, ctx) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
+
+
+    stack = [vyxalify(item) for item in [["abc", "def", "ghi"]]]
+    expected = vyxalify(["adg", "beh", "cfi"])
+    ctx = Context()
+
+    ctx.stacks.append(stack)
+
+    code = transpile('∩')
+    # print('∩', code)
+    exec(code)
+
+    ctx.stacks.pop()
+    actual = vyxalify(stack[-1])
+
+    print(simplify(expected), simplify(actual))
+
+    if vy_type(actual, simple=True) is list or vy_type(expected, simple=True) is list:
+        assert all(deep_flatten(equals(actual, expected, ctx), ctx)) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
+    else:
+        assert equals(actual, expected, ctx) or non_vectorising_equals(actual, expected, ctx), "Expected " + str(expected) + ", got " + str(simplify(actual))
+
+
+    stack = [vyxalify(item) for item in [["abcde", "fgh"]]]
+    expected = vyxalify(["af", "bg", "ch", "d", "e"])
     ctx = Context()
 
     ctx.stacks.append(stack)
