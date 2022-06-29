e4da3928be27c9a3dce105714c74f461d585d19f
Caleb Miller <caleb@mathmammoth.com>
2022-06-29 16:11:59 -0600

    Add CI to sort elements in elements.py

---
+++ sort_element_functions.py
@@ -0,0 +1,17 @@
+import re
+
+f = open('vyxal/elements.py', 'r', encoding='utf-8')
+file = f.read()
+f.close()
+
+fns = [x.strip() for x in re.findall("(?:@[a-zA-Z_]+\n)?def +[a-zA-Z_][a-zA-Z_\d]*\s*\([\S\s]*?\)(?: *-> *.+?)?\:(?:\n(?:(?:    |\t).*)?)+", file)[1:]]
+
+file = file.split("\n")
+
+start_line = file.index(fns[0].split("\n")[0])
+last_line = file.index(fns[-1].split("\n")[-1])
+
+f = open('vyxal/elements.py', 'w', encoding='utf-8')
+
+f.write("\n".join(file[:start_line]) + "\n" + "\n\n\n".join(sorted(fns, key=lambda x: x[x.index('def '):])) + "\n".join(file[last_line + 1:]))
+f.close()
