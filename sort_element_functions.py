b812362fcdedaa40ad3265bdd0898c71e8c9024f
Caleb Miller <caleb@mathmammoth.com>
2022-06-29 16:17:34 -0600

    oops

--- sort_element_functions.py
+++ sort_element_functions.py
@@ -13,5 +13,5 @@ last_line = file.index(fns[-1].split("\n")[-1])
 
 f = open('vyxal/elements.py', 'w', encoding='utf-8')
 
-f.write("\n".join(file[:start_line]) + "\n" + "\n\n\n".join(sorted(fns, key=lambda x: x[x.index('def '):])) + "\n".join(file[last_line + 1:]))
+f.write("\n".join(file[:start_line]) + "\n" + "\n\n\n".join(sorted(fns, key=lambda x: x[x.index('def '):])) + "\n\n" + "\n".join(file[last_line + 1:]))
 f.close()
