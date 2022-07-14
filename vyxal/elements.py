8ca17876b967961b45a896c4f1fc6cb96e3c0879
Chunkybanana <62921243+chunkybanana@users.noreply.github.com>
2022-07-14 05:38:25 +0000

    Added letter-to-number

--- vyxal/elements.py
+++ vyxal/elements.py
@@ -2678,6 +2678,18 @@ def less_than_or_equal(lhs, rhs, ctx):
     }.get(ts, lambda: vectorise(less_than_or_equal, lhs, rhs, ctx=ctx))()
 
 
+def letter_to_number(lhs, ctx):
+    """Element øA
+    (num) -> number_to_letter(a)
+    (str) -> letter_to_number(a)
+    """
+    ts = vy_type(lhs)
+    return {
+        NUMBER_TYPE: lambda: chr(lhs + 96),
+        str: lambda: ord(lhs) - 96,
+    }.get(ts, lambda: vectorise(letter_to_number, lhs, ctx=ctx))()
+
+
 def lift(lhs, ctx):
     """Element Þż
     (any) -> a * 1...a.length
@@ -6630,6 +6642,7 @@ elements: dict[str, tuple[str, int]] = {
     "øm": process_element(vertical_mirror_center_join, 1),
     "øṀ": process_element(flip_brackets_vertical_mirror, 1),
     "øW": process_element(group_on_words, 1),
+    "øA": process_element(letter_to_number, 1),
     "øP": process_element(pluralise_count, 2),
     "øp": process_element(starts_with, 2),
     "øE": process_element(ends_with, 2),
