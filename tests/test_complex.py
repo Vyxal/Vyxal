32ddacc827b06995a202f85d758f57067f413395
lyxal <36217120+Lyxal@users.noreply.github.com>
2022-09-04 22:35:05 +1000

    Helps to make sure infinite lists are indexed properly!

--- tests/test_complex.py
+++ tests/test_complex.py
@@ -921,16 +921,16 @@ def test_complex_numbers():
 
 def test_all_powers():
     stack = run_vyxal("4 ¨e")
-    assert stack[-1] == [4, 16, 64, 256, 1024, 4096, 16384, 65536]
+    assert stack[-1][:8] == [4, 16, 64, 256, 1024, 4096, 16384, 65536]
 
     stack = run_vyxal("¨²")
-    assert stack[-1] == [2, 4, 8, 16, 32, 64, 128, 256]
+    assert stack[-1][:8] == [2, 4, 8, 16, 32, 64, 128, 256]
 
     stack = run_vyxal("¨ε")
-    assert stack[-1] == [3, 9, 27, 81, 243, 729, 2187, 6561]
+    assert stack[-1][:8] == [3, 9, 27, 81, 243, 729, 2187, 6561]
 
     stack = run_vyxal("¨₀")
-    assert stack[-1] == [
+    assert stack[-1][:8] == [
         10,
         100,
         1000,
