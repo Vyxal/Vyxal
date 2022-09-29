10d969175e7c59cfc94d4a3bfc7b759b1a6ee782
lyxal <36217120+Lyxal@users.noreply.github.com>
2022-09-29 23:49:52 +1000

    Update lexer.scala

--- src/main/scala/lexer.scala
+++ src/main/scala/lexer.scala
@@ -1,23 +1,33 @@
 import scala.util.parsing.combinator._
 
 sealed trait VyxalToken
-trait WorkflowCompilationError
-case class WorkflowLexerError(msg: String) extends WorkflowCompilationError
+trait VyxalCompilationError
+case class VyxalLexerError(msg: String) extends VyxalCompilationError
 
 case class NUMBER(value: String) extends VyxalToken
 case class STRING(value: String) extends VyxalToken
+case class STRUCTURE_OPEN(value: String) extends VyxalToken
 case class COMMAND(value: String) extends VyxalToken
+case class DIGRAPH(value: String) extends VyxalToken
+
+val CODEPAGE : String = """ᵃᵇᶜᵈᵉᶠᶢᴴᶤᶨᵏᶪᵐⁿᵒᵖᴿᶳᵗᵘᵛᵂᵡᵞᶻᶴ′″‴⁴ᵜ !"#$%&'()*+,-./0123456789:;
+<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~¦ȦḂĊḊĖḞĠḢİĿṀṄ
+ȮṖṘṠṪẆẊικȧḃċḋėḟġḣŀṁṅȯṗṙṡṫẋƒΘΦ§ẠḄḌḤỊḶṂṆỌṚṢṬ…≤≥≠₌⁺⁻⁾√∑«»⌐∴∵⊻₀₁₂₃₄₅₆₇₈₉λƛΩ₳µ∆øÞ½ʀɾ¯
+×÷£¥←↑→↓±‡†Π¬∧∨⁰¹²³¤¨∥∦ı„”ð€“¶ᶿᶲ•≈¿ꜝ"""
+
 
 object lexer extends RegexParsers {
     override def skipWhitespace = true
     def number: Parser[NUMBER] = """[0-9]+""".r ^^ { case value => NUMBER(value) }
     def string: Parser[STRING] = """"[^"]*"""".r ^^ { case value => STRING(value.substring(1, value.length - 1)) }
-    def command: Parser[COMMAND] = """[a-zA-Z+\-*/]""".r ^^ { case value => COMMAND(value) }
+    def structureOpen: Parser[STRUCTURE_OPEN] = """[\[\(\{λƛΩ₳µ]|#@""".r ^^ { case value => STRUCTURE_OPEN(value) }
+    def digraph: Parser[DIGRAPH] = s"[∆øÞ#][$CODEPAGE]".r ^^ { case value => DIGRAPH(value) }
+    def command: Parser[COMMAND] = s"[$CODEPAGE]".r ^^ { case value => COMMAND(value) }
 
-    def tokens: Parser[List[VyxalToken]] = phrase(rep1(number | string | command)) ^^ { case tokens => tokens }
-    def apply(code: String): Either[WorkflowLexerError, List[VyxalToken]] = {
+    def tokens: Parser[List[VyxalToken]] = phrase(rep1(number | string | command | structureOpen)) ^^ { case tokens => tokens }
+    def apply(code: String): Either[VyxalLexerError, List[VyxalToken]] = {
     parse(tokens, code) match {
-      case NoSuccess(msg, next) => Left(WorkflowLexerError(msg))
+      case NoSuccess(msg, next) => Left(VyxalLexerError(msg))
       case Success(result, next) => Right(result)
     }
   }
