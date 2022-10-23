4cdeb80bf603bc59f73a4d4e37bc677466be7973
ysthakur <2004yst@gmail.com>
2022-10-22 21:04:58 -0400

    Change parser a bit (doesn't compile)

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -1,5 +1,6 @@
 import scala.util.parsing.combinator.Parsers
 import scala.util.parsing.input.{Reader, Position, NoPosition}
+import scala.collection.mutable.ListBuffer
 
 class VyxalTokenReader(tokens: Seq[VyxalToken]) extends Reader[VyxalToken] {
   override def first: VyxalToken = tokens.head
@@ -10,43 +11,56 @@ class VyxalTokenReader(tokens: Seq[VyxalToken]) extends Reader[VyxalToken] {
 
 object VyxalParser extends Parsers {
   type Elem = VyxalToken
+
   def parseAll: Parser[List[AST]] = phrase(rep1(element))
-  def element: Parser[AST] = number | string | command | structure | partialStructure
-  def number: Parser[AST] = accept("number", { case VyxalToken.Number(value) => AST.Number(value) })
-  def string: Parser[AST] = accept("string", { case VyxalToken.Str(value) => AST.Str(value) })
-  def command: Parser[AST] = accept("command", { case VyxalToken.Command(value) => AST.Command(value) })
+  def nonStructElement: Parser[AST] = number | string | command
+  def element: Parser[AST] = nonStructElement | structure
+  def number: Parser[AST] =
+    accept("number", { case VyxalToken.Number(value) => AST.Number(value) })
+  def string: Parser[AST] =
+    accept("string", { case VyxalToken.Str(value) => AST.Str(value) })
+  def command: Parser[AST] =
+    accept("command", { case VyxalToken.Command(value) => AST.Command(value) })
   def structure: Parser[AST] = {
     val open = accept("open", { case VyxalToken.StructureOpen(open) => open })
-    val close = accept("close", { case VyxalToken.StructureClose(close) => close })
-    open ~ rep(element) ~ (VyxalToken.Branch ~ rep(element)).? ~ close.? ^^ {
-    case open ~ elements ~ Some(VyxalToken.Branch ~ elements2) ~ close => AST.Structure(open, List(elements, elements2))
-    case open ~ elements ~ None ~ close => AST.Structure(open, List(elements))
-  }}
-  def partialStructure: Parser[AST] = {
-    val open = accept("open", { case VyxalToken.StructureOpen(open) => open })
-    rep1(open ~ rep(element) ~ (VyxalToken.Branch ~ rep(element)).?) ~ VyxalToken.StructureAllClose ^^ {
-      case lhs ~ VyxalToken.StructureAllClose => AST.Structure(lhs.head._1, lhs.map(_._2))
+    val close =
+      accept("close", { case VyxalToken.StructureClose(close) => close })
+    open ~ rep(nonStructElement) ~ (VyxalToken.Branch ~ rep(
+      nonStructElement
+    )).? ~ close.? ^^ {
+      case open ~ elements ~ Some(VyxalToken.Branch ~ elements2) ~ close =>
+        AST.Structure(open, List(elements, elements2))
+      case open ~ elements ~ None ~ close => AST.Structure(open, List(elements))
     }
   }
-  
+  def partialStructure: Parser[AST] = {
+    val open = accept("open", { case VyxalToken.StructureOpen(open) => open })
+    rep1(
+      open ~ rep(element) ~ (VyxalToken.Branch ~ rep(element)).?
+    ) ~ VyxalToken.StructureAllClose ^^ {
+      case lhs ~ VyxalToken.StructureAllClose =>
+        AST.Structure(lhs.head._1, lhs.map(_._2))
+    }
+  }
 
   def apply(tokens: Seq[VyxalToken]): List[AST] = {
     val reader = new VyxalTokenReader(preprocess(tokens))
     parseAll(reader) match {
-    case Success(result, _) => result
-    case NoSuccess(msg, _) => throw new Exception(msg)
-  }
-}}
-
-def preprocess(code: Seq[VyxalToken]) : List[VyxalToken] = {
-  var processed = List[VyxalToken]()
-  code.foreach(x => {
-    x match {
-      case VyxalToken.StructureClose(")") => {
-        processed = processed :+ VyxalToken.StructureClose("}")
-        processed = processed :+ VyxalToken.StructureClose("}")
-      }
-      case _ => processed = processed :+ x
+      case Success(result, _) => result
+      case NoSuccess(msg, _)  => throw new Exception(msg)
     }
-  })
-  processed}
+  }
+}
+
+def preprocess(code: Seq[VyxalToken]): List[VyxalToken] = {
+  val processed = ListBuffer[VyxalToken]()
+
+  code.foreach {
+    case VyxalToken.StructureClose(")") => {
+      processed += VyxalToken.StructureClose("}")
+      processed += VyxalToken.StructureClose("}")
+    }
+    case x => processed += x
+  }
+  processed.toList
+}
