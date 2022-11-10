0724679e29a8c6bc63c22aa641f1bf27394d676a
ysthakur <2004yst@gmail.com>
2022-11-09 19:32:11 -0500

    Copy flag descriptions from Vyxal 2

--- shared/src/main/scala/Context.scala
+++ shared/src/main/scala/Context.scala
@@ -2,6 +2,45 @@ package vyxal
 
 import scala.collection.{mutable => mut}
 
+/** What kind of implicit output is wanted at the end
+  */
+enum EndPrintMode {
+
+  /** Just print the top of the stack */
+  case Default
+  case JoinNewlines
+
+  /** Join on newlines vertically */
+  case JoinNewlinesVert
+
+  /** Sum/concatenate the top of the stack */
+  case Sum
+}
+
+/** Settings set by flags
+  *
+  * @param presetStack
+  *   Whether to push a 100 onto the stack before the program starts
+  */
+case class Settings(
+    presetStack: Boolean = false,
+    endPrintMode: EndPrintMode = EndPrintMode.Default
+) {
+
+  /** Add a flag to these settings
+    *
+    * @return
+    *   A Some with the new settings, or None if the flag is invalid
+    */
+  def withFlag(flag: Char): Settings = flag match {
+    case 'H' => this.copy(presetStack = true)
+    case 'j' => this.copy(endPrintMode = EndPrintMode.JoinNewlines)
+    case 'L' => this.copy(endPrintMode = EndPrintMode.JoinNewlinesVert)
+    case 's' => this.copy(endPrintMode = EndPrintMode.Sum)
+    case _   => throw IllegalArgumentException(s"$flag is an invalid flag")
+  }
+}
+
 /** @constructor
   *   Make a Context object for the current scope
   * @param vars
