0ee0382bfa00133a4c7c6075c51e8598a9fc1375
lyxal <36217120+Lyxal@users.noreply.github.com>
2023-01-01 14:58:42 +1100

    Implement chunk-while
    Closes #1432

--- vyxal/elements.py
+++ vyxal/elements.py
@@ -5984,6 +5984,7 @@ def vy_divmod(lhs, rhs, ctx):
     (num, num) -> [lhs // rhs, lhs % rhs]
     (iterable, num) -> combinations of a with length b
     (str, str) ->  overwrite the start of a with b
+    (lst, fun) -> group elements in a by elements that fulfil predicate b
     """
     ts = vy_type(lhs, rhs, simple=True)
 
@@ -6001,6 +6002,9 @@ def vy_divmod(lhs, rhs, ctx):
         (str, str): lambda: rhs + lhs[len(rhs) :],
         (list, NUMBER_TYPE): lambda: combinations(lhs, rhs),
         (NUMBER_TYPE, list): lambda: combinations(rhs, lhs),
+        (list, types.FunctionType): lambda: chunk_while(lhs, rhs, ctx),
+        (types.FunctionType, list): lambda: chunk_while(rhs, lhs, ctx),
+
     }.get(ts, lambda: vectorise(vy_divmod, lhs, rhs, ctx=ctx))()
 
 
