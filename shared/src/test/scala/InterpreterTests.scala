f4ece4da7b33edbaa97879b6887e778ee6a52f8d
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-01-27 13:26:05 -0500

    Finish converting InterpreterTests

--- shared/src/test/scala/InterpreterTests.scala
+++ shared/src/test/scala/InterpreterTests.scala
@@ -1,6 +1,7 @@
 package vyxal
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.Checkpoints.Checkpoint
 import spire.math.{Complex, Real}
 
 class InterpreterTests extends VyxalTests:
@@ -129,95 +130,122 @@ class InterpreterTests extends VyxalTests:
         }
       }
     }
+    describe("Variable unpacking") {
+      it("should handle non nested lists") {
+        given ctx: Context = Context()
+        Interpreter.execute("#[1 | 2 | 3#] #:[x|y|z]")
+        val cp = Checkpoint()
+        cp { assertResult(VNum(1))(ctx.getVar("x")) }
+        cp { assertResult(VNum(2))(ctx.getVar("y")) }
+        cp { assertResult(VNum(3))(ctx.getVar("z")) }
+        cp.reportAll()
+      }
+      it("should handle nested lists") {
+        given ctx: Context = Context()
+        Interpreter.execute("#[1 | 2 | #[3#]#] #:[x|y|z]")
+        val cp = Checkpoint()
+        cp { assertResult(VNum(1))(ctx.getVar("x")) }
+        cp { assertResult(VNum(2))(ctx.getVar("y")) }
+        cp { assertResult(VList(3))(ctx.getVar("z")) }
+        cp.reportAll()
+      }
+      it("should handle simple nested patterns") {
+        given ctx: Context = Context()
+        Interpreter.execute("#[1 | 2 | #[3#]#] #:[x|y|[z]]")
+        val cp = Checkpoint()
+        cp { assertResult(VNum(1))(ctx.getVar("x")) }
+        cp { assertResult(VNum(2))(ctx.getVar("y")) }
+        cp { assertResult(VNum(3))(ctx.getVar("z")) }
+        cp.reportAll()
+      }
+    }
+    describe("Nonlocal variables") {
+      it("should be able to get nonlocal variables inside lambdas") {
+        given ctx: Context = Context()
+        ctx.setVar("x", 5)
+        ctx.push(1)
+        assertResult(VNum(5))(
+          Interpreter.executeFn(
+            VFun.fromLambda(AST.Lambda(1, Nil, AST.GetVar("x")))
+          )
+        )
+      }
+      it("should be able to set nonlocal variables inside lambdas?") {
+        given ctx: Context = Context()
+        ctx.setVar("x", 5)
+        ctx.push(1)
+        Interpreter.executeFn(
+          VFun.fromLambda(
+            AST.Lambda(1, Nil, AST.AugmentVar("x", AST.Command("+")))
+          )
+        )
+        assertResult(VNum(6))(ctx.getVar("x"))
+      }
+      it(
+        "should resolve to variables from original ctx inside of current ctx"
+      ) {
+        val ctx1 = Context()
+        ctx1.setVar("x", 5)
+        Interpreter.execute(
+          AST.Lambda(1, Nil, AST.AugmentVar("x", AST.Command("+")))
+        )(using ctx1)
+        val ctx2 = Context()
+        ctx2.setVar("x", "foo")
+        ctx2.push(1)
+        ctx2.push(ctx1.pop()) // Push the lambda defined earlier
+        Interpreter.execute(AST.ExecuteFn)(using ctx2)
+        assertResult((VNum(6), "foo"))((ctx1.getVar("x"), ctx2.getVar("x")))
+      }
+    }
   }
 
-  // todo convert the rest of these tests
-  // test("Does the interpreter handle variable unpacking?") {
-  //   given ctx: Context = Context()
-  //   Interpreter.execute("#[1 | 2 | 3#] #:[x|y|z]")
-  //   assert(ctx.getVar("x") == VNum(1))
-  //   assert(ctx.getVar("y") == VNum(2))
-  //   assert(ctx.getVar("z") == VNum(3))
-
-  //   Interpreter.execute("#[1 | 2 | #[3#]#] #:[x|y|z]")
-  //   assert(ctx.getVar("x") == VNum(1))
-  //   assert(ctx.getVar("y") == VNum(2))
-  //   assert(ctx.getVar("z") == VList(3))
-
-  //   Interpreter.execute("#[1 | 2 | #[3#]#] #:[x|y|[z]]")
-  //   assert(ctx.getVar("x") == VNum(1))
-  //   assert(ctx.getVar("y") == VNum(2))
-  //   assert(ctx.getVar("z") == VNum(3))
-  // }
-
-  // test("Can the interpreter get nonlocal variables inside lambdas?") {
-  //   given ctx: Context = Context()
-  //   ctx.setVar("x", 5)
-  //   ctx.push(1)
-  //   assertResult(VNum(5))(
-  //     Interpreter.executeFn(
-  //       VFun.fromLambda(AST.Lambda(1, Nil, AST.GetVar("x")))
-  //     )
-  //   )
-  // }
-
-  // test("Can the interpreter set nonlocal variables inside lambdas?") {
-  //   given ctx: Context = Context()
-  //   ctx.setVar("x", 5)
-  //   ctx.push(1)
-  //   Interpreter.executeFn(
-  //     VFun.fromLambda(AST.Lambda(1, Nil, AST.AugmentVar("x", AST.Command("+"))))
-  //   )
-  //   assertResult(VNum(6))(ctx.getVar("x"))
-  // }
-
-  // test("References to variables in other contexts") {
-  //   val ctx1 = Context()
-  //   ctx1.setVar("x", 5)
-  //   Interpreter.execute(
-  //     AST.Lambda(1, Nil, AST.AugmentVar("x", AST.Command("+")))
-  //   )(using ctx1)
-  //   val ctx2 = Context()
-  //   ctx2.setVar("x", "foo")
-  //   ctx2.push(1)
-  //   ctx2.push(ctx1.pop())
-  //   Interpreter.execute(AST.ExecuteFn)(using ctx2)
-  //   assertResult((VNum(6), "foo"))((ctx1.getVar("x"), ctx2.getVar("x")))
-  // }
-
-  // todo come up with a way to easily have multiple assertResults in one group
-  // and still run them all
+  // todo come up with an easier way to run multiple assertions without using
+  // Checkpoint
   describe("Numeric literals") {
     it("should parse simple integers correctly") {
-      assertResult(VNum(0))(VNum.from("0"))
-      assertResult(VNum(1))(VNum.from("1"))
-      assertResult(VNum(BigInt("123456789123456789")))(
-        VNum.from("123456789123456789")
-      )
+      val cp = Checkpoint()
+      cp { assertResult(VNum(0))(VNum.from("0")) }
+      cp { assertResult(VNum(1))(VNum.from("1")) }
+      cp {
+        assertResult(VNum(BigInt("9999999999999999999")))(
+          VNum.from("9999999999999999999")
+        )
+      }
+      cp.reportAll()
     }
     it("should parse decimals correctly") {
-      assertResult(VNum(Real("6.9")))(VNum.from("6.9"))
-      assertResult(VNum(Real("0.9")))(VNum.from("0.9"))
-      assertResult(VNum(Real("0.9")))(VNum.from(".9"))
+      val cp = Checkpoint()
+      cp { assertResult(VNum(Real("6.9")))(VNum.from("6.9")) }
+      cp { assertResult(VNum(Real("0.9")))(VNum.from("0.9")) }
+      cp { assertResult(VNum(Real("0.9")))(VNum.from(".9")) }
+      cp.reportAll()
     }
     it("should handle trailing dots correctly") {
-      assertResult(VNum(Real("0.5")))(VNum.from("."))
-      assertResult(VNum(Real("0.5")))(VNum.from("0."))
-      assertResult(VNum(Real("5.5")))(VNum.from("5."))
+      val cp = Checkpoint()
+      cp { assertResult(VNum(Real("0.5")))(VNum.from(".")) }
+      cp { assertResult(VNum(Real("0.5")))(VNum.from("0.")) }
+      cp { assertResult(VNum(Real("5.5")))(VNum.from("5.")) }
+      cp.reportAll()
     }
     it("should handle complex numbers correctly") {
-      assertResult(VNum.complex(0, 1))(VNum.from("ı"))
-      assertResult(VNum.complex(0, 1))(VNum.from("0ı"))
-      assertResult(VNum.complex(0.5, 1))(VNum.from("0.ı"))
-      assertResult(VNum.complex(0.5, 0.5))(VNum.from(".ı."))
-      assertResult(VNum.complex(69, 420))(VNum.from("69ı420"))
+      val cp = Checkpoint()
+      cp { assertResult(VNum.complex(0, 1))(VNum.from("ı")) }
+      cp { assertResult(VNum.complex(0, 1))(VNum.from("0ı")) }
+      cp { assertResult(VNum.complex(0.5, 1))(VNum.from("0.ı")) }
+      cp { assertResult(VNum.complex(0.5, 0.5))(VNum.from(".ı.")) }
+      cp { assertResult(VNum.complex(69, 420))(VNum.from("69ı420")) }
+      cp.reportAll()
     }
     it("should handle digits too large for the radix") {
-      assertResult(VNum(23))(VNum.from("A3", 2))
+      val cp = Checkpoint()
+      cp { assertResult(VNum(23))(VNum.from("A3", 2)) }
+      cp.reportAll()
     }
     it("should handle invalid characters") {
-      assertResult(VNum(Real("12.3")))(VNum.from("1@#$%2#$%. 3"))
-      assertResult(VNum(Real(0)))(VNum.from("@#  `/$%#$% "))
+      val cp = Checkpoint()
+      cp { assertResult(VNum(Real("12.3")))(VNum.from("1@#$%2#$%. 3")) }
+      cp { assertResult(VNum(Real(0)))(VNum.from("@#  `/$%#$% ")) }
+      cp.reportAll()
     }
   }
 
