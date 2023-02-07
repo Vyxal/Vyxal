e5457076cca5de92ac3d2f9f661001b91549d472
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-02-07 12:31:44 +1100

    Create LiterateLexer.scala

---
+++ shared/src/main/scala/LiterateLexer.scala
@@ -0,0 +1,39 @@
+package vyxal
+
+import java.util.regex.Pattern
+import scala.util.matching.Regex
+import scala.util.parsing.combinator.*
+import LiterateToken.*
+
+enum LiterateToken(val value: String):
+  case Word(override val value: String) extends LiterateToken(value)
+  case AlreadyCode(override val value: String) extends LiterateToken(value)
+  case LitComment(override val value: String) extends LiterateToken(value)
+
+object LiterateLexer extends RegexParsers:
+  override def skipWhitespace = true
+  override val whiteSpace: Regex = "[ \t\r\f]+".r
+
+  private def decimalRegex = raw"((0|[1-9][0-9]*)?\.[0-9]*|0|[1-9][0-9]*)"
+  def number: Parser[LiterateToken] =
+    raw"($decimalRegex?ı$decimalRegex?)|$decimalRegex".r ^^ { value =>
+      AlreadyCode(value)
+    }
+
+  def string: Parser[LiterateToken] = raw"""("(?:[^"\\]|\\.)*["])""".r ^^ {
+    value => Word(value.replaceAll("\\\\\"", "\""))
+  }
+
+  def singleCharString: Parser[LiterateToken] = """'.""".r ^^ { value =>
+    Word(value)
+  }
+
+  def comment: Parser[LiterateToken] = """##[^\n]*""".r ^^ { value =>
+    LitComment(value)
+  }
+
+  def lambdaBlock: Parser[LiterateToken] = // everything between balanced {}s
+    """\{""".r ~ rep(lambdaBlock | """[^{}]+""".r) ~ """\}""".r ^^ {
+      case _ ~ body ~ _ => Word(body.mkString(""))
+    }
+end LiterateLexer
