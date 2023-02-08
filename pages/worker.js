3d6109b55ffd559fd744efa068ff5848126d8662
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-02-09 00:00:54 +1100

    Update worker.js

--- pages/worker.js
+++ pages/worker.js
@@ -1,4 +1,4 @@
-importScripts("https://raw.githubusercontent.com/Vyxal/Vyxal/v3-online-interpreter/pages/worker.js")
+
 self.addEventListener('message', function (e) {
     var data = e.data;
     Vyxal.execute(data.code, data.inputs, data.flags)
