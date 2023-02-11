36c0bd9116a93ee8c87e56c68302eb59f535013e
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-02-10 21:15:51 -0500

    Copy new literate-handling CLI/REPL back

--- shared/src/main/scala/cli/CLI.scala
+++ shared/src/main/scala/cli/CLI.scala
@@ -25,7 +25,9 @@ object CLI:
       code: Option[String] = None,
       inputs: List[String] = List.empty,
       printDocs: Boolean = false,
+      printLiterate: Boolean = false,
       printHelp: Boolean = false,
+      runLiterate: Boolean = false,
       settings: Settings = Settings()
   )
 
@@ -44,6 +46,10 @@ object CLI:
           printDocs()
           return
 
+        if config.printLiterate then
+          printLiterateMap()
+          return
+
         config.file.foreach { file =>
           val source = io.Source.fromFile(config.file.get)
           try
@@ -53,11 +59,12 @@ object CLI:
         }
 
         config.code.foreach { code =>
-          Interpreter.execute(code)
+          if config.runLiterate then Interpreter.runLiterate(code)
+          else Interpreter.execute(code)
         }
 
         if config.file.nonEmpty || config.code.nonEmpty then return
-        else Repl.startRepl()
+        else Repl.startRepl(config.runLiterate)
       case None => ???
     end match
   end run
@@ -84,11 +91,49 @@ object CLI:
           print(
             s"$symbol ($name) (${if vectorises then "" else "non-"}vectorising)\n"
           )
+          println(s"Keywords:${keywords.mkString(" ", ", ", "")}")
           overloads.foreach { overload =>
             println(s"- $overload")
           }
           println("---------------------")
       }
+
+    Modifiers.modifiers.foreach { case (name, info) =>
+      print(s"$name\n")
+      println(s"Keywords:${info.keywords.mkString(" ", ", ", "")}")
+      println(s"Description: ${info.description}")
+      println("---------------------")
+    }
+  end printDocs
+
+  private def printLiterateMap(): Unit =
+    println("package vyxal\n")
+    println("val literateModeMappings = Map(\n")
+    Elements.elements.values.toSeq
+      .sortBy { elem =>
+        // Have to use tuple in case of digraphs
+        (
+          vyxal.CODEPAGE.indexOf(elem.symbol.charAt(0)),
+          vyxal.CODEPAGE.indexOf(elem.symbol.substring(1))
+        )
+      }
+      .foreach {
+        case Element(
+              symbol,
+              name,
+              keywords,
+              arity,
+              vectorises,
+              overloads,
+              impl
+            ) =>
+          for keyword <- keywords do println(s"""  "$keyword" -> "$symbol",""")
+      }
+    Modifiers.modifiers.foreach { case (name, info) =>
+      for keyword <- info.keywords do println(s"""  "$keyword" -> "$name",""")
+    }
+    println(")")
+  end printLiterateMap
 
   private val builder = OParser.builder[CLIConfig]
 
@@ -101,6 +146,7 @@ object CLI:
         .text(text)
         .optional()
 
+    // todo come up with better names for the flags
     OParser.sequence(
       programName("vyxal"),
       head("vyxal", "3.1.1"),
@@ -119,6 +165,14 @@ object CLI:
       opt[Unit]('d', "docs")
         .action((_, cfg) => cfg.copy(printDocs = true))
         .text("Print documentation for elements and exit")
+        .optional(),
+      opt[Unit]('D', "docsLiterate")
+        .action((_, cfg) => cfg.copy(printLiterate = true))
+        .text("Print literate mode mappings and exit")
+        .optional(),
+      opt[Unit]('l', "literate")
+        .action((_, cfg) => cfg.copy(runLiterate = true))
+        .text("Enable literate mode")
         .optional(),
       arg[String]("<input>...")
         .unbounded()
@@ -187,7 +241,7 @@ object CLI:
       flag('O', "disable-implicit-output", "Disable implicit output"),
       flag('o', "force-implicit-output", "Force implicit output"),
       flag(
-        'l',
+        '!',
         "print-length",
         "Print length of top of stack on end of execution"
       ),
--- shared/src/main/scala/cli/Repl.scala
+++ shared/src/main/scala/cli/Repl.scala
@@ -1,13 +1,13 @@
 package vyxal.cli
 
 import vyxal.{Context, Interpreter}
-
 import scala.io.StdIn
 
 object Repl:
-  def startRepl()(using ctx: Context): Unit =
+  def startRepl(literate: Boolean = false)(using ctx: Context): Unit =
     while true do
       print("> ")
 
       val code = StdIn.readLine()
-      Interpreter.execute(code)
+      if literate then Interpreter.runLiterate(code)
+      else Interpreter.execute(code)
