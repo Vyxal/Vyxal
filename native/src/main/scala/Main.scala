438334ff538e322d3cf1eed8cadb327a072f7b33
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-02-11 23:09:32 +1100

    Also make scala native compile

--- native/src/main/scala/Main.scala
+++ native/src/main/scala/Main.scala
@@ -4,4 +4,8 @@ import vyxal.cli.CLI
 
 object Main:
   def main(args: Array[String]): Unit =
-    CLI.run(args)
+    if args.nonEmpty && args(0).charAt(0) != '-' then
+      val source = io.Source.fromFile(args(0))
+      CLI.run(args, Some(source.mkString))
+    else CLI.run(args, None)
+    end if
