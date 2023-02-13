66ad97c53dd6f98b3a1f65e1f8fc55e6cc57d858
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-02-13 22:37:55 +1100

    Make the linter happy

--- shared/src/main/scala/Elements.scala
+++ shared/src/main/scala/Elements.scala
@@ -329,8 +329,8 @@ object Elements:
       "a: num, b: num -> a in base b - list of digits",
       "a: num, b: str|lst -> a in base with alphabet b",
     ) {
-      case (a: VFun, b: VAny) => ListHelpers.filter(b, a)
-      case (a: VAny, b: VFun) => ListHelpers.filter(a, b)
+      case (a: VFun, b) => ListHelpers.filter(b, a)
+      case (a, b: VFun) => ListHelpers.filter(a, b)
     }
 
     val getContextVariableM = addNilad(
