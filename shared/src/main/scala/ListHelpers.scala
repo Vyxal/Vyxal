6d8370ff40c40c1a990889ae08d462d41dd40351
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-02-14 14:22:00 +1100

    Update shared/src/main/scala/ListHelpers.scala
    Co-authored-by: ysthakur <45539777+ysthakur@users.noreply.github.com>

--- shared/src/main/scala/ListHelpers.scala
+++ shared/src/main/scala/ListHelpers.scala
@@ -61,14 +61,13 @@ object ListHelpers:
         return VList(to.zipWithIndex.map { (item, index) =>
           f.execute(item, index, List(item))
         }*)
-    var temp: VList = to
-    for branch <- branches do
-      temp = VList(temp.zipWithIndex.map { (item, index) =>
+    branches.foldLeft(to) { (mapped, branch) =>
+      VList(mapped.zipWithIndex.map { (item, index) =>
         VFun
           .fromLambda(AST.Lambda(1, List.empty, List(branch)))
           .execute(item, index, List(item))
       }*)
-    temp
+    }
   end map
 
   /** Mold a list into a shape.
