c3bd4649bd86e4d3aabff088f490119f253c6e5d
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-02-16 02:46:30 -0500

    Fix syntax error
    :|

--- shared/src/main/scala/Elements.scala
+++ shared/src/main/scala/Elements.scala
@@ -270,7 +270,7 @@ object Elements:
       "a: str -> Evaluate a as Vyxal",
       "a: num -> 10 ** n"
     ) { ctx ?=>
-      ctx.push(execHelper(ctx.pop())
+      ctx.push(execHelper(ctx.pop()))
     }
 
     def execHelper(value: VAny)(using ctx: Context): VAny =
@@ -280,7 +280,7 @@ object Elements:
           ctx.pop()
         case n: VNum     => 10 ** n
         case list: VList => list.vmap(execHelper)
-        case fn: VFun =>
+        case fn: VFun    =>
           ctx.push(Interpreter.executeFn(fn))
           if fn.arity == -1 then
             ctx.pop() // Handle the extra value pushed by lambdas that operate on the stack
