0577b46b9da796386b26373c7af9cfb46acc6d2a
mathcat4 <tams.mathe@gmail.com>
2023-02-21 14:56:33 +0100

    well this doesn't seem right

--- vyxal/elements.py
+++ vyxal/elements.py
@@ -2855,8 +2855,6 @@ def index_indices_or_cycle(lhs, rhs, ctx):
             first = True
             while True:
                 curr = deep_copy(safe_apply(rhs, curr, ctx=ctx))
-                if curr == lhs and not first:
-                    break
                 if curr in prevs:
                     yield from prevs
                     break
