560cd6a461788dc8f864858f8ad4d198874389a8
ysthakur <ysthakur@users.noreply.github.com>
2023-03-07 00:49:54 +0000

    Scalafix changes

--- shared/src/main/scala/VNum.scala
+++ shared/src/main/scala/VNum.scala
@@ -34,7 +34,8 @@ class VNum private (val underlying: Complex[Real]):
   override def toString =
     def toStringHelper(n: Real): String =
       if n.isWhole then n.toString
-      else n.floor.toString + "." + (n - n.floor).toDouble.toString().substring(2)
+      else
+        n.floor.toString + "." + (n - n.floor).toDouble.toString().substring(2)
     if this.imag == 0 then toStringHelper(this.real)
     else toStringHelper(this.real) + "ı" + toStringHelper(this.imag)
 
