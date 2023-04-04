2d6021d38880ad70b38d8f4e01d4aa8f673e0929
The Thonnu <55329600+nayakrujul@users.noreply.github.com>
2023-04-04 16:23:20 +0100

    That failed. Let's try again

--- vyxal/helpers.py
+++ vyxal/helpers.py
@@ -1248,7 +1248,7 @@ def transpose(
     while True:
         if any(has_ind(row, r) for row in matrix):
             this_row = gen_row(r)
-            if all(type(row[r]) is str for row in matrix):
+            if all(type(x) is str for x in this_row):
                 yield "".join(this_row)
             else:
                 yield this_row
