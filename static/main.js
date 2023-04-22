6987166ca2a53b4ab6938af75648f677188c8840
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-04-22 11:21:35 +1000

    Update static/main.js
    Co-authored-by: Steffan <40404519+Steffan153@users.noreply.github.com>

--- static/main.js
+++ static/main.js
@@ -639,7 +639,7 @@ function shareOptions(shareType) {
             output = `[Vyxal, ${len} byte${"s".repeat(code.length != 1)}${utfable ? '' : ' (UTF-8)'}](${url})`
             break
         case "post-template":
-            output = `#[Vyxal](https://github.com/Vyxal/Vyxal)${flagAppendage} ${len} byte${"s".repeat(len != 1)}${utfable ? '' : ' (UTF-8)'}
+            output = `# [Vyxal](https://github.com/Vyxal/Vyxal)${flagAppendage} ${len} byte${"s".repeat(len != 1)}${utfable ? '' : ' (UTF-8)'}
 \`\`\`
 ${code}
 \`\`\`
