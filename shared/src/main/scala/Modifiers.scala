f41bedd120a0c4bc11242129a258e207bf3af90c
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-05-24 16:08:09 +1000

    Update Modifiers.scala

--- shared/src/main/scala/Modifiers.scala
+++ shared/src/main/scala/Modifiers.scala
@@ -49,10 +49,10 @@ object Modifiers:
       val lambdaAst = astToLambda(ast, ast.arity.getOrElse(2))
       AST.makeSingle(lambdaAst, AST.Command("R"))
     },
-    "ߣ" -> Modifier(
+    "⸠" -> Modifier(
       "Single Element Lambda",
       """|Turn the next element (whether that be a structure/modifier/element) into a lambda
-         |⥑f: Push the equivalent of λf} to the stack""".stripMargin,
+         |⸠f: Push the equivalent of λf} to the stack""".stripMargin,
       List("*-"),
       1
     ) { case List(ast) => AST.makeSingle(astToLambda(ast, 1)) },
