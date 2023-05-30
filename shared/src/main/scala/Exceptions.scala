4c09a1ecb77aeec729542b5959d8af71e0f87660
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-05-30 14:12:13 -0400

    Also test JS in test workflow

--- shared/src/main/scala/Exceptions.scala
+++ shared/src/main/scala/Exceptions.scala
@@ -5,15 +5,27 @@ case class UnimplementedOverloadException(element: String, args: Seq[VAny])
       s"$element not supported for input(s) ${args.mkString("(", ", ", ")")}"
     )
 
+/** Exception to end program using element Q */
 class QuitException extends RuntimeException("Program quit")
+
+/** Exception to signal that a loop should continue. Should technically never be
+  * unhandled.
+  */
 class ContinueLoopException
     extends RuntimeException(
       "Tried to continue outside of a loop context"
-    ) // Should technically never be unhandled
+    )
+
+/** Exception to signal that a loop should break. Should technically never be
+  * unhandled.
+  */
 class BreakLoopException
     extends RuntimeException(
       "Tried to break outside of a loop context"
     ) // Should technically never be unhandled
 
+/** Exception to signal that a function should return. Should technically never
+  * be unhandled.
+  */
 class ReturnFromFunctionException
     extends RuntimeException("Tried to return outside of a function context")
