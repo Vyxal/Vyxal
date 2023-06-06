5f683519bc234b69b07120ca2b9e240304436a2f
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-06-07 00:10:17 +1000

    Update NumberHelpers.scala

--- shared/src/main/scala/NumberHelpers.scala
+++ shared/src/main/scala/NumberHelpers.scala
@@ -147,7 +147,7 @@ object NumberHelpers:
     if value == VNum(0) then VList(List(VNum(0))*)
     else if base.toBigInt == -1 then
       VList.from(
-        Seq.fill(value.toInt.abs)(Seq(1, 0)).flatten.dropRight(1).map(VNum(_))
+        Seq.fill(value.toInt.abs)(Seq(1, 0)).flatten.map(VNum(_))
       )
     else
       val sign = if value.toBigInt < 0 && base.toBigInt > 0 then -1 else 1
