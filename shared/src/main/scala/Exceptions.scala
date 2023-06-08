96e0b63923d9fd9e68f846fb57be5a83ea5d3b6e
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-06-08 09:07:19 -0400

    Make RecursionError parameter not a field

--- shared/src/main/scala/Exceptions.scala
+++ shared/src/main/scala/Exceptions.scala
@@ -8,26 +8,20 @@ case class UnimplementedOverloadException(element: String, args: Seq[VAny])
 /** Exception to end program using element Q */
 class QuitException extends RuntimeException("Program quit")
 
-/** Exception to signal that a loop should continue. Should technically never be
-  * unhandled.
+/** Exception to signal that a loop should continue. Should never be unhandled.
   */
 class ContinueLoopException
-    extends RuntimeException(
-      "Tried to continue outside of a loop context"
-    )
+    extends RuntimeException("Tried to continue outside of a loop context")
 
-/** Exception to signal that a loop should break. Should technically never be
-  * unhandled.
+/** Exception to signal that a loop should break. Should never be unhandled.
   */
 class BreakLoopException
-    extends RuntimeException(
-      "Tried to break outside of a loop context"
-    ) // Should technically never be unhandled
+    extends RuntimeException("Tried to break outside of a loop context")
 
-/** Exception to signal that a function should return. Should technically never
-  * be unhandled.
+/** Exception to signal that a function should return. Should never be
+  * unhandled.
   */
 class ReturnFromFunctionException
     extends RuntimeException("Tried to return outside of a function context")
 
-class RecursionError(val message: String) extends RuntimeException(message)
+class RecursionError(message: String) extends RuntimeException(message)
