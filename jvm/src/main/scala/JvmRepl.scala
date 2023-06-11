71855a678d9d584af9cdaf4baf24ca679186943e
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-06-11 19:43:54 -0400

    Enable debug logging

--- jvm/src/main/scala/JvmRepl.scala
+++ jvm/src/main/scala/JvmRepl.scala
@@ -4,13 +4,19 @@ import org.jline.reader.EndOfFileException
 import org.jline.reader.LineReaderBuilder
 import org.jline.reader.UserInterruptException
 import org.jline.terminal.TerminalBuilder
+import java.util.logging.Logger
+import java.util.logging.Level
 
 object JvmRepl:
   def startRepl(literate: Boolean)(using Context): Unit =
+    // Enable debug logging
+    Logger.getLogger("org.jline").setLevel(Level.FINE)
+
     val terminal = TerminalBuilder
       .builder()
       .name("vyxal")
       .system(true)
+      // .streams(System.in, System.out)
       .build()
     val lineReader = LineReaderBuilder
       .builder()
