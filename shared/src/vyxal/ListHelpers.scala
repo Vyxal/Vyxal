17184635ed514fa66b5c0a3850057136526471cf
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-08-09 08:52:45 +1000

    Update shared/src/vyxal/ListHelpers.scala
    Co-authored-by: ysthakur <45539777+ysthakur@users.noreply.github.com>

--- shared/src/vyxal/ListHelpers.scala
+++ shared/src/vyxal/ListHelpers.scala
@@ -372,7 +372,7 @@ object ListHelpers:
   end sortBy
 
   def sum(lst: VList)(using ctx: Context): VAny =
-    lst.foldLeft(MiscHelpers.defaultEmpty(lst.head))(_ +~ _)
+    if lst.isEmpty then ctx.settings.defaultValue else lst.reduce(_ +~ _)
 
   def prefixes(iterable: VList): Seq[VList] =
     iterable.inits.toSeq.reverse.tail
