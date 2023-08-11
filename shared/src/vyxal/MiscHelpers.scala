39b7a2494d0519ce8b145db0b9094b82cb6acc0c
lyxal <36217120+Lyxal@users.noreply.github.com>
2023-08-11 23:03:55 +1000

    Add tests for `ȧ` + switch to `FuncHelpers.fill`

--- shared/src/vyxal/MiscHelpers.scala
+++ shared/src/vyxal/MiscHelpers.scala
@@ -9,10 +9,11 @@ import scala.collection.mutable.Stack
 
 object MiscHelpers:
 
-  val absoluteDifference = Dyad.vectorise("absoluteDifference") {
+  val absoluteDifference: Dyad = Dyad.fill("absoluteDifference") {
     case (a: VNum, b: VNum) => (a - b).vabs
-    case (a: VList, b: VNum) => a.vmap(x => (x.asInstanceOf[VNum] - b).vabs)
-    case (a: VNum, b: VList) => b.vmap(x => (a - x.asInstanceOf[VNum]).vabs)
+    case (a: VList, b: VNum) => a.vmap(MiscHelpers.absoluteDifference(_, b))
+    case (a: VNum, b: VList) => b.vmap(MiscHelpers.absoluteDifference(a, _))
+    case (a: VList, b: VList) => a.zipWith(b)(MiscHelpers.absoluteDifference)
     case (a: VList, b: VFun) =>
       VList.from(ListHelpers.overlaps(a, 2).map(x => b(x*)))
     case (a: VFun, b: VList) =>
