361a263ebe0e11966019a8083c348fab0a416021
The Thonnu <55329600+nayakrujul@users.noreply.github.com>
2023-08-19 19:24:46 +0100

    Update shared/src/vyxal/NumberHelpers.scala
    Co-authored-by: ysthakur <45539777+ysthakur@users.noreply.github.com>

--- shared/src/vyxal/NumberHelpers.scala
+++ shared/src/vyxal/NumberHelpers.scala
@@ -147,7 +147,7 @@ object NumberHelpers:
       else i += 1
     VList.from(result.toList)
 
-  def primeFactorsUniquified(a: VNum)(using ctx: Context): VList =
+  def uniquePrimeFactors(a: VNum)(using Context): VList =
     ListHelpers.uniquify(NumberHelpers.primeFactors(a))
 
   def range(start: VNum, end: VNum): VList =
