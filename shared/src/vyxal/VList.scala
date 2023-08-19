4bc390038723d28c98f16011b9a2f0573bfb246f
nayakrujul <nayakrujul@hotmail.com>
2023-08-19 19:43:29 +0100

    I'm just guessing here lol

--- shared/src/vyxal/VList.scala
+++ shared/src/vyxal/VList.scala
@@ -198,13 +198,13 @@ end VList
   override def distinct(using Context): VList =
     val uniq: LazyList[Option[VAny]] =
       LazyList.unfold(Seq[VAny]() -> 0) { state =>
-        if !iter.hasIndex(state._2) then None
-        else if state._1.contains(iter.index(state._2)) then
+        if !Seq.hasIndex(state._2) then None
+        else if state._1.contains(Seq.index(state._2)) then
           Some(None, state._1 -> (state._2 + 1))
         else
           Some(
-            Some(iter.index(state._2)),
-            (state._1 :+ iter.index(state._2)) -> (state._2 + 1)
+            Some(Seq.index(state._2)),
+            (state._1 :+ Seq.index(state._2)) -> (state._2 + 1)
           )
       }
 
