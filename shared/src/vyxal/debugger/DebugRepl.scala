61056f62fc4ecc2d60dadef19840d9eaae9f3993
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-08-23 02:05:16 -0400

    feat: Add breakpoints

--- shared/src/vyxal/debugger/DebugRepl.scala
+++ shared/src/vyxal/debugger/DebugRepl.scala
@@ -31,6 +31,17 @@ object DebugRepl:
               case Cmd.Continue =>
                 dbg.continue()
                 printState(dbg)
+              case Cmd.Resume =>
+                dbg.resume()
+                printState(dbg)
+              case Cmd.AddBreakpoint(offset, label) =>
+                dbg.addBreakpoint(Breakpoint(offset, label)())
+              case Cmd.RemoveBreakpointByOffset(offset) =>
+                dbg.removeBreakpoint(offset)
+              case Cmd.RemoveBreakpointByLabel(label) =>
+                dbg.removeBreakpoint(label)
+              case Cmd.ListBreakpoints =>
+                println(dbg.getBreakpoints.mkString("\n"))
               case Cmd.Frames =>
                 for frame <- dbg.stackFrames do
                   println(s"<${frame.name}> ${frame.ast}")
@@ -71,7 +82,45 @@ object DebugRepl:
         .text("Step out"),
       cmd("continue")
         .action((_, cfg) => cfg.copy(cmd = Cmd.Continue))
-        .text("Continue execution to end of program, ignoring breakpoints"),
+        .text("Continue to the next breakpoint"),
+      cmd("resume")
+        .action((_, cfg) => cfg.copy(cmd = Cmd.Resume))
+        .text("Resume execution to end of program, ignoring breakpoints"),
+      cmd("add")
+        .text("Add a breakpoint")
+        .children(
+          arg[Int]("<offset>")
+            .action((offset, cfg) =>
+              cfg.copy(cmd = Cmd.AddBreakpoint(offset, None))
+            )
+            .text("The offset for the breakpoint to add"),
+          arg[String]("<label>")
+            .action((label, cfg) =>
+              val Cmd.AddBreakpoint(offset, _) = cfg.cmd: @unchecked
+              cfg.copy(cmd = Cmd.AddBreakpoint(offset, Some(label)))
+            )
+            .optional()
+            .text("The label for the breakpoint")
+        ),
+      cmd("remove")
+        .text("Remove a breakpoint using either its offset or label")
+        .children(
+          opt[Int]('o', "offset")
+            .action((offset, cfg) =>
+              cfg.copy(cmd = Cmd.RemoveBreakpointByOffset(offset))
+            )
+            .text("The offset for the breakpoint to remove")
+            .optional(),
+          opt[String]('l', "label")
+            .action((label, cfg) =>
+              cfg.copy(cmd = Cmd.RemoveBreakpointByLabel(label))
+            )
+            .text("The label for the breakpoint to remove")
+            .optional(),
+        ),
+      cmd("list")
+        .action((_, cfg) => cfg.copy(cmd = Cmd.ListBreakpoints))
+        .text("List all breakpoints"),
       cmd("frames")
         .action((_, cfg) => cfg.copy(cmd = Cmd.Frames))
         .text("Show all the frames"),
@@ -102,6 +151,11 @@ object DebugRepl:
     case StepOver
     case StepOut
     case Continue
+    case Resume
+    case AddBreakpoint(offset: Int, label: Option[String])
+    case RemoveBreakpointByOffset(offset: Int)
+    case RemoveBreakpointByLabel(label: String)
+    case ListBreakpoints
     case Frames
     case Eval(code: String)
     case Stack(n: Int)
