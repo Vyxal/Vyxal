374998d8635a3ad597bdee8d10b848392bd50bcd
lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-16 14:17:07 +1100

    Exclude worker.js and main.js from being sent over

--- pages/filter_files.py
+++ pages/filter_files.py
@@ -4,4 +4,5 @@ if __name__ == "__main__":
     files = [
         x for x in sys.argv[1].split("\n") if x.endswith("js") or x.endswith("map")
     ]
+    files = [x for x in files if not x in ["worker.js", "main.js"]]
     print(" !! ".join(files))
