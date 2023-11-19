77ddbe491f6978d6512111c3e08af4a472186865
lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-19 23:01:28 +1100

    Change a test, and remove some things marked as modifiers

--- shared/src/vyxal/parsing/SBCSLexer.scala
+++ shared/src/vyxal/parsing/SBCSLexer.scala
@@ -128,7 +128,7 @@ private[parsing] object SBCSLexer extends Lexer:
   def command[$: P]: P[Token] = parseToken(Command, CharPred(allCommands).!)
 
   def monadicModifier[$: P]: P[Token] =
-    parseToken(MonadicModifier, CharIn("ᵃᵇᶜᵈᴴᶤᶨᵏᶪᵐⁿᵒᵖᴿᶳᵗᵘᵛᵂᵡᵞᶻ¿⸠/~@`").!)
+    parseToken(MonadicModifier, CharIn("ᵃᵇᶜᵈᴴᶤᶨᵏᶪᵐⁿᵒᵖᴿᶳᵗᵘᵛᵂᵡᵞᶻ¿⸠/@").!)
 
   def dyadicModifier[$: P]: P[Token] =
     parseToken(DyadicModifier, CharIn("ϩ∥∦ᵉ").!)
