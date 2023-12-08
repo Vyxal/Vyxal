5350eec346998720d1fb05ba49200f4982bab4e5
Aaron Miller <57011988+AMiller42@users.noreply.github.com>
2023-12-07 23:42:41 -0500

    Update tests

--- shared/test/src/vyxal/InterpreterTests.scala
+++ shared/test/src/vyxal/InterpreterTests.scala
@@ -410,6 +410,16 @@ class InterpreterTests extends VyxalTests:
         )
       }
     }
+    describe("Base-252 compression") {
+      it("should not remove leading 'a' characters") {
+        val compressed = StringHelpers.compress252("aabbcc sussybaka")
+        assertResult("aabbcc sussybaka")(
+          StringHelpers.decompress252String(
+            compressed.substring(1, compressed.length - 1)
+          )
+        )
+      }
+    }
   }
 
   describe("Global Recursion (Factorial)") {
