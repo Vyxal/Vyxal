9e33ece485b37ae84a8962d9d1757d9265046a00
GingerIndustries <GingerIndustries@users.noreply.github.com>
2023-12-13 21:31:25 +0000

    Scalafix changes

--- jvm/src/vyxal/gen/GenerateTheseusData.scala
+++ jvm/src/vyxal/gen/GenerateTheseusData.scala
@@ -1,14 +1,17 @@
 package vyxal.gen
 
 import vyxal.parsing.Lexer
-import upickle.default.*
 import vyxal.Modifiers
 
+import upickle.default.*
+
 private object GenerateTheseusData:
-    private def wrap(data: String) = f"""// This is a generated file, do not edit by hand
+  private def wrap(data: String) =
+    f"""// This is a generated file, do not edit by hand
 const GENERATED=${data};
 export default GENERATED;
     """
 
-    def codepage(): String = wrap(upickle.default.write(Lexer.Codepage))
-    def modifiers(): String = wrap(upickle.default.write(Modifiers.modifiers.keys.toList))
+  def codepage(): String = wrap(upickle.default.write(Lexer.Codepage))
+  def modifiers(): String =
+    wrap(upickle.default.write(Modifiers.modifiers.keys.toList))
