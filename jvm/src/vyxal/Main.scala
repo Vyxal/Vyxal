0268e84a50ca4eda2a21dc7dbdf28f4184f1c98d
AMiller42 <AMiller42@users.noreply.github.com>
2023-12-14 22:04:21 +0000

    Scalafix changes

--- jvm/src/vyxal/Main.scala
+++ jvm/src/vyxal/Main.scala
@@ -2,8 +2,7 @@ package vyxal
 
 object Main:
   def main(args: Array[String]): Unit =
-    if args.contains("--fuzz") then
-      Fuzz.fuzz(args(1).toInt, args(2).toInt)
+    if args.contains("--fuzz") then Fuzz.fuzz(args(1).toInt, args(2).toInt)
     else
       try CLI.run(args, JvmRepl)
       catch
