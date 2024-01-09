ccd26d3b01a7d0c243a95848c2975baa420fdbfb
Madeline Vergani <RubenVerg@outlook.com>
2024-01-09 10:51:06 +0100

    Change list evaluation to collecting all pushed items on a new stack

--- shared/src/vyxal/Context.scala
+++ shared/src/vyxal/Context.scala
@@ -202,6 +202,9 @@ class Context private (
 
   def rotateRight: Unit = stack.insert(0, pop())
 
+  def clear(): Unit =
+    stack.clear()
+
   def copy: Context =
     new Context(
       mut.ArrayBuffer().addAll(stack),
