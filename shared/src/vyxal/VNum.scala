1788f1009e6a5d1653b22ea602826a95dc8244f1
pacmanboss256 <pacmanboss972@gmail.com>
2024-01-09 15:36:38 -0600

    dont fix a thing that wasnt just added again

--- shared/src/vyxal/VNum.scala
+++ shared/src/vyxal/VNum.scala
@@ -24,8 +24,10 @@ class VNum private (val underlying: Complex[Real]) extends Ordered[VNum]:
   /** Whether the real part is small enough to be converted to an `Int` */
   def isValidInt: Boolean = underlying.real.isValidInt
 
-  /** Whether there is an imaginary part */
-  def isComplex: Boolean = underlying.imag != 0
+  /** Whether there is an imaginary and a real part */
+  def isComplex: Boolean = (underlying.imag != 0 && underlying.real != 0)
+  /** Whether there is only an imaginary part */
+  def isImaginary: Boolean = (underlying.imag != 0 && underlying.real == 0)
 
   /** Round the real and imaginary parts */
   def toIntegral: VNum = underlying.round
