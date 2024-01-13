55339a2d7434ec646697d81a9477df5e212d1585
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-01-13 23:23:04 +1100

    fix duplicate names

--- shared/src/vyxal/parsing/Vexer.scala
+++ shared/src/vyxal/parsing/Vexer.scala
@@ -1,33 +1,33 @@
-package vyxal
+package vyxal.parsing
 
-case class Token(
-    tokenType: TokenType,
+case class VToken(
+    tokenType: VTokenType,
     value: String,
     range: Range,
 ) derives CanEqual:
   override def equals(obj: Any): Boolean =
     obj match
-      case other: Token => (other `eq` this) ||
+      case other: VToken => (other `eq` this) ||
         (other.tokenType == this.tokenType && other.value == this.value)
 
       case _ => false
 
   override def toString: String = s"$tokenType(\"$value\")"
 
-case class LitToken(
-    tokenType: TokenType,
-    value: String | List[LitToken],
+case class VLitToken(
+    tokenType: VTokenType,
+    value: String | List[VLitToken],
     range: Range,
 ) derives CanEqual:
   override def equals(obj: Any): Boolean =
     obj match
-      case other: LitToken => (other `eq` this) ||
+      case other: VLitToken => (other `eq` this) ||
         (other.tokenType == this.tokenType &&
           (other.value match
             case otherValue: String => otherValue ==
                 this.value.asInstanceOf[String]
-            case otherValue: List[LitToken] => otherValue ==
-                this.value.asInstanceOf[List[LitToken]]
+            case otherValue: List[VLitToken] => otherValue ==
+                this.value.asInstanceOf[List[VLitToken]]
           ))
 
       case _ => false
@@ -37,7 +37,7 @@ case class LitToken(
 /** The range of a token or AST in the source code. The start offset is
   * inclusive, the end offset is exclusive.
   */
-case class Range(startOffset: Int, endOffset: Int) derives CanEqual:
+case class VRange(startOffset: Int, endOffset: Int) derives CanEqual:
   def includes(offset: Int): Boolean =
     startOffset <= offset && offset < endOffset
 
@@ -46,27 +46,27 @@ case class Range(startOffset: Int, endOffset: Int) derives CanEqual:
     */
   override def equals(obj: Any): Boolean =
     obj match
-      case other: Range => (other `eq` this) ||
-        (this `eq` Range.fake) ||
-        (other `eq` Range.fake) ||
+      case other: VRange => (other `eq` this) ||
+        (this `eq` VRange.fake) ||
+        (other `eq` VRange.fake) ||
         (other.startOffset == this.startOffset &&
           other.endOffset == this.endOffset)
       case _ => false
 
-object Range:
+object VRange:
   /** A dummy Range (mainly for generated/desugared code) */
-  val fake: Range = Range(-1, -1)
+  val fake: VRange = VRange(-1, -1)
 
-enum TokenType(val canonicalSBCS: Option[String] = None) extends Enum[TokenType]
-    derives CanEqual:
+enum VTokenType(val canonicalSBCS: Option[String] = None)
+    extends Enum[VTokenType] derives CanEqual:
   case Number
   case Str
   case StructureOpen
-  case StructureClose extends TokenType(Some("}"))
-  case StructureDoubleClose extends TokenType(Some(")"))
-  case StructureAllClose extends TokenType(Some("]"))
-  case ListOpen extends TokenType(Some("#["))
-  case ListClose extends TokenType(Some("#]"))
+  case StructureClose extends VTokenType(Some("}"))
+  case StructureDoubleClose extends VTokenType(Some(")"))
+  case StructureAllClose extends VTokenType(Some("]"))
+  case ListOpen extends VTokenType(Some("#["))
+  case ListClose extends VTokenType(Some("#]"))
   case Command
   case Digraph
   case UnpackTrigraph
@@ -91,10 +91,10 @@ enum TokenType(val canonicalSBCS: Option[String] = None) extends Enum[TokenType]
   case Constant
   case AugmentVar
   case UnpackVar
-  case Branch extends TokenType(Some("|"))
-  case Newline extends TokenType(Some("\n"))
+  case Branch extends VTokenType(Some("|"))
+  case Newline extends VTokenType(Some("\n"))
   case Param
-  case UnpackClose extends TokenType(Some("]"))
+  case UnpackClose extends VTokenType(Some("]"))
   case GroupType
   case NegatedCommand
   case MoveRight
@@ -103,34 +103,34 @@ enum TokenType(val canonicalSBCS: Option[String] = None) extends Enum[TokenType]
   /** Helper to help go from the old VyxalToken to the new Token(TokenType,
     * text, range) format
     */
-  def apply(text: String): Token = Token(this, text, Range.fake)
+  def apply(text: String): VToken = VToken(this, text, Range.fake)
 
   /** Helper to destructure tokens more concisely */
-  def unapply(tok: Token): Option[(String | List[Token], Range)] =
+  def unapply(tok: VToken): Option[(String | List[VToken], Range)] =
     if tok.tokenType == this then Some((tok.value, tok.range)) else None
-end TokenType
+end VTokenType
 
-enum StructureType(val open: String) derives CanEqual:
-  case Ternary extends StructureType("[")
-  case While extends StructureType("{")
-  case For extends StructureType("(")
-  case Lambda extends StructureType("λ")
-  case LambdaMap extends StructureType("ƛ")
-  case LambdaFilter extends StructureType("Ω")
-  case LambdaReduce extends StructureType("₳")
-  case LambdaSort extends StructureType("µ")
-  case IfStatement extends StructureType("#{")
-  case DecisionStructure extends StructureType("Ḍ")
-  case GeneratorStructure extends StructureType("Ṇ")
-  case DefineStructure extends StructureType("#::")
+enum VStructureType(val open: String) derives CanEqual:
+  case Ternary extends VStructureType("[")
+  case While extends VStructureType("{")
+  case For extends VStructureType("(")
+  case Lambda extends VStructureType("λ")
+  case LambdaMap extends VStructureType("ƛ")
+  case LambdaFilter extends VStructureType("Ω")
+  case LambdaReduce extends VStructureType("₳")
+  case LambdaSort extends VStructureType("µ")
+  case IfStatement extends VStructureType("#{")
+  case DecisionStructure extends VStructureType("Ḍ")
+  case GeneratorStructure extends VStructureType("Ṇ")
+  case DefineStructure extends VStructureType("#::")
 
-object StructureType:
-  val lambdaStructures: List[StructureType] = List(
-    StructureType.Lambda,
-    StructureType.LambdaMap,
-    StructureType.LambdaFilter,
-    StructureType.LambdaReduce,
-    StructureType.LambdaSort,
+object VStructureType:
+  val lambdaStructures: List[VStructureType] = List(
+    VStructureType.Lambda,
+    VStructureType.LambdaMap,
+    VStructureType.LambdaFilter,
+    VStructureType.LambdaReduce,
+    VStructureType.LambdaSort,
   )
 
 object Vexer {}
