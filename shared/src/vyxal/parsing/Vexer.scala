44e5ede3b42b9197ddd2ccb0a401ba76719a93a3
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-01-18 22:35:15 +1100

    Update Vexer.scala

--- shared/src/vyxal/parsing/Vexer.scala
+++ shared/src/vyxal/parsing/Vexer.scala
@@ -172,8 +172,8 @@ class Vexer(val program: String = ""):
       if headIsDigit || headEqual('.') then numberToken
       else if headIsWhitespace then pop(1)
       else if headEqual('"') then stringToken
-      else if headIn("∆øÞ") || headLookaheadMatch("#[^[\\]$!=#>@{:]") then
-        digraphToken
+      else if headIn("∆øÞ") || headLookaheadMatch("""#[^\[\]$!=#>@{:.,^]""")
+      then digraphToken
       else if headLookaheadMatch("#[.,^]") then sugarTrigraph
       else
         val rangeStart = index
