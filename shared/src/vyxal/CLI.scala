2676dcae47c3491cbeda187a1f8c9ba5fb76e19f
Madeline Vergani <RubenVerg@outlook.com>
2024-01-30 18:42:58 +0100

    messed something up, I think it's now fixed

--- shared/src/vyxal/CLI.scala
+++ shared/src/vyxal/CLI.scala
@@ -23,32 +23,18 @@ object FlagCategory:
 enum Flag(
     val short: Char,
     val long: String,
-    val helpText: String,
     val description: String,
     val category: Option[FlagCategory] = None,
     val hidden: Boolean = false,
 ) extends Enum[Flag]:
   case Trace
-      extends Flag(
-        'X',
-        "trace",
-        "Return full traceback on program error",
-        "Full traceback",
-      )
-  case Preset100
-      extends Flag(
-        'H',
-        "preset-100",
-        "Preset stack to 100",
-        "Preset stack to 100",
-      )
-  case Literate
-      extends Flag('l', "literate", "Enable literate mode", "Literate mode")
+      extends Flag('X', "trace", "Return full traceback on program error")
+  case Preset100 extends Flag('H', "preset-100", "Preset stack to 100")
+  case Literate extends Flag('l', "literate", "Enable literate mode")
   case RangeNone
       extends Flag(
         '\u0000',
         "",
-        "Default behavior",
         "Default behavior",
         Some(FlagCategory.RangeBehavior),
         true,
@@ -58,7 +44,6 @@ enum Flag(
         'M',
         "range-start-0",
         "Make implicit range generation and while loop counter start at 0 instead of 1",
-        "Start range at 0",
         Some(FlagCategory.RangeBehavior),
       )
   case RangeEndExcl
@@ -66,7 +51,6 @@ enum Flag(
         'm',
         "range-end-excl",
         "Make implicit range generation end at n-1 instead of n",
-        "End range at n-1",
         Some(FlagCategory.RangeBehavior),
       )
   case RangeProgrammery
@@ -74,29 +58,21 @@ enum Flag(
         'Ṁ',
         "range-programmery",
         "Equivalent to having both m and M flags",
-        "Both",
         Some(FlagCategory.RangeBehavior),
       )
   case InputAsStrings
-      extends Flag(
-        'Ṡ',
-        "inputs-as-strs",
-        "Treat all inputs as strings",
-        "Don't evaluate inputs",
-      )
+      extends Flag('Ṡ', "inputs-as-strs", "Treat all inputs as strings")
   case NumbersAsRanges
       extends Flag(
         'R',
         "numbers-as-ranges",
         "Treat numbers as ranges if ever used as an iterable",
-        "Rangify",
       )
   case Arity1
       extends Flag(
         '\u0000',
         "",
         "Make the default arity of lambdas 1",
-        "1",
         Some(FlagCategory.DefaultArity),
         true,
       )
@@ -105,7 +81,6 @@ enum Flag(
         '2',
         "arity-2",
         "Make the default arity of lambdas 2",
-        "2",
         Some(FlagCategory.DefaultArity),
       )
   case Arity3
@@ -113,7 +88,6 @@ enum Flag(
         '3',
         "arity-3",
         "Make the default arity of lambdas 3",
-        "3",
         Some(FlagCategory.DefaultArity),
       )
   case LimitOutput
@@ -121,7 +95,6 @@ enum Flag(
         '…',
         "limit-output",
         "Limit list output to the first 100 items of that list",
-        "Limit list output",
       )
 
   case PrintTop
@@ -129,7 +102,6 @@ enum Flag(
         '\u0000',
         "",
         "Print the top of the stack",
-        "Default behavior",
         Some(FlagCategory.EndPrintMode),
         true,
       )
@@ -138,7 +110,6 @@ enum Flag(
         'j',
         "print-join-newlines",
         "Print top of stack joined by newlines on end of execution",
-        "Join top with newlines",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintSum
@@ -146,7 +117,6 @@ enum Flag(
         's',
         "print-sum",
         "Sum/concatenate top of stack on end of execution",
-        "Sum/concatenate top",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintDeepSum
@@ -154,7 +124,6 @@ enum Flag(
         'd',
         "print-deep-sum",
         "Print deep sum of top of stack on end of execution",
-        "Deep sum of top",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintJoinSpaces
@@ -162,7 +131,6 @@ enum Flag(
         'S',
         "print-join-spaces",
         "Print top of stack joined by spaces on end of execution",
-        "Join top with spaces",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintNone
@@ -170,14 +138,12 @@ enum Flag(
         'O',
         "disable-implicit-output",
         "Disable implicit output",
-        "No implicit output",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintForce
       extends Flag(
         'o',
         "force-implicit-output",
-        "Force implicit output",
         "Force implicit output",
         Some(FlagCategory.EndPrintMode),
       )
@@ -186,7 +152,6 @@ enum Flag(
         'L',
         "print-length",
         "Print length of top of stack on end of execution",
-        "Length of top",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintPretty
@@ -194,7 +159,6 @@ enum Flag(
         '§',
         "print-pretty",
         "Print top of stack pretty-printed on end of execution",
-        "Pretty-print top",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintMax
@@ -202,7 +166,6 @@ enum Flag(
         'G',
         "print-max",
         "Print the maximum item of the top of stack on end of execution",
-        "Maximum of top",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintMin
@@ -210,16 +173,14 @@ enum Flag(
         'g',
         "print-min",
         "Print the minimum item of the top of the stack on end of execution",
-        "Minimum of top",
         Some(FlagCategory.EndPrintMode),
       )
-  // case PrintAll extends Flag('W', "print-all", "Print the entire stack on end of execution", "All the stack", Some(FlagCategory.EndPrintMode))
+  // case PrintAll extends Flag('W', "print-all", "Print the entire stack on end of execution", Some(FlagCategory.EndPrintMode))
   case PrintSumAll
       extends Flag(
         'Ṫ',
         "print-sum-all",
         "Print the sum of the entire stack",
-        "Sum of stack",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintStackLength
@@ -227,7 +188,6 @@ enum Flag(
         '!',
         "print-stack-length",
         "Print the length of the stack on end of execution",
-        "Length of stack",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintAllJoinSpaces
@@ -235,16 +195,14 @@ enum Flag(
         'ṡ',
         "print-all-join-spaces",
         "Print the entire stack, joined on spaces",
-        "Join stack with spaces",
         Some(FlagCategory.EndPrintMode),
       )
-  // case PrintAllJoinNewlines extends Flag('J', "print-all-join-newlines", "Print the entire stack, separated by newlines", "Join stack with newlines", Some(FlagCategory.EndPrintMode))
+  // case PrintAllJoinNewlines extends Flag('J', "print-all-join-newlines", "Print the entire stack, separated by newlines", Some(FlagCategory.EndPrintMode))
   case PrintAllJoinNothing
       extends Flag(
         'N',
         "print-all-join-nothing",
         "Print the entire stack, concatenated",
-        "Join stack with nothing",
         Some(FlagCategory.EndPrintMode),
       )
   case PrintNot
@@ -252,7 +210,6 @@ enum Flag(
         '¬',
         "logical-not",
         "Logically negate the top of the stack on end of execution",
-        "Logical negation of top",
         Some(FlagCategory.EndPrintMode),
       )
 end Flag
@@ -434,13 +391,6 @@ object CLI:
   private val parser =
     import builder.*
 
-    /** Helpers for adding flags that go into Settings */
-    def flag(short: Char, name: String, text: String) =
-      opt[Unit](short, name)
-        .action((_, cfg) => cfg.copy(settings = cfg.settings.withFlag(short)))
-        .text(text)
-        .optional()
-
     // todo come up with better names for the flags
     OParser.sequence(
       programName("vyxal"),
@@ -502,7 +452,7 @@ object CLI:
               .action((_, cfg) =>
                 cfg.copy(settings = cfg.settings.withFlag(f.short))
               )
-              .text(f.helpText)
+              .text(f.description)
               .optional()
         })*
     )
