8eb71f3aace01410e3b3fc327aa6bb90c0f28cb7
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-09-04 23:35:19 +1000

    Interpreter 3.4.5 for permalinks

--- shared/src/vyxal/Interpreter.scala
+++ shared/src/vyxal/Interpreter.scala
@@ -9,7 +9,7 @@ import scala.collection.mutable.ListBuffer
 import scala.collection.mutable as mut
 
 object Interpreter:
-  def version = "3.4.4"
+  def version = "3.4.5"
   def execute(code: String)(using ctx: Context): Unit =
     /** Attempt lexing */
     val tokens =
