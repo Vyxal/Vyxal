f1d7a78deb59a400507af5b6d3012c6fe9e5f99a
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-09-14 01:02:34 +1000

    Version bumpies again!

--- shared/src/vyxal/Interpreter.scala
+++ shared/src/vyxal/Interpreter.scala
@@ -9,7 +9,7 @@ import scala.collection.mutable.ListBuffer
 import scala.collection.mutable as mut
 
 object Interpreter:
-  def version = "3.4.7"
+  def version = "3.4.8"
   def execute(code: String)(using ctx: Context): Unit =
 
     /** Attempt lexing */
