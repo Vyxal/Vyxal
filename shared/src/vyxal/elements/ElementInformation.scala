3ab9a5bd7ad9518d85ce691cd187ffa3bd1ba3ff
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-10-21 23:23:29 +1100

    Update ElementInformation.scala

--- shared/src/vyxal/elements/ElementInformation.scala
+++ shared/src/vyxal/elements/ElementInformation.scala
@@ -1,1 +1,19 @@
 package vyxal.elements
+
+case class Element(
+    symbol: String,
+    name: String,
+    keywords: Seq[String],
+    arity: Option[Int],
+    vectorises: Boolean,
+    overloads: Seq[String],
+)
+
+object ElementInformation:
+  def symbolFor(keyword: String): Option[String] =
+    ElementInformation.elements.values
+      .find(_.keywords.contains(keyword))
+      .map(_.symbol)
+
+  val elements: Map[String, Element] = Map(
+  )
