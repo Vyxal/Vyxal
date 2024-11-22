91752ea164ad9182da37452fa759e38e365b5fdd
lyxal <36217120+lyxal@users.noreply.github.com>
2024-11-22 23:22:46 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     version="0.1",
     description="Vyxal 2 executable",
     options={"build_exe": build_exe_options},
-    executables=[Executable("vyxal/__main__.py", base="console")],
+    executables=[Executable("vyxal/__main__.py", base="console", target_name="vyxal2")],
 )
