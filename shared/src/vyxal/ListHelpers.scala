588d057fcdf2dba4fb7fe72683ca15186fc3a1dc
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-12-13 00:54:38 +1100

    Rotating things I love rotating things it's very intuative

--- shared/src/vyxal/ListHelpers.scala
+++ shared/src/vyxal/ListHelpers.scala
@@ -901,21 +901,21 @@ object ListHelpers:
     */
   def rotate(iterable: VAny, amount: VNum): VAny =
     var counter = 0
-    val direction = if amount < 0 then -1 else 1
+    val direction = if amount < 0 then -1 else 1 // -1 for right, 1 for left
     val amountInt = amount.vabs
     var temp = iterable
 
     while counter <= amountInt do
       temp = temp match
         case list: VList =>
-          if direction == -1 then VList.from(list.tail :+ list.head)
+          if direction == 1 then VList.from(list.tail :+ list.head)
           else VList.from(list.last +: list.init)
         case str: String =>
-          if direction == -1 then str.tail + str.head
+          if direction == 1 then str.tail + str.head
           else s"${str.last}${str.init}"
         case num: VNum =>
           val str = num.toString
-          if direction == -1 then VNum(str.tail + str.head)
+          if direction == 1 then VNum(str.tail + str.head)
           else VNum(s"${str.last}${str.init}")
         case _ => throw BadArgumentException("rotate", iterable)
       counter += 1
--- shared/test/src/vyxal/ElementTests.scala
+++ shared/test/src/vyxal/ElementTests.scala
@@ -309,14 +309,14 @@ class ElementTests extends VyxalTests:
 
   describe("Element ↜") {
     testStackLike("↜")(
-      in(1, 2, 3) -> List[VAny](2, 3, 1),
+      in(1, 2, 3) -> List[VAny](1, 3, 2),
       in(1) -> List[VAny](1),
     )
   }
 
   describe("Element ↝") {
     testStackLike("↝")(
-      in(1, 2, 3) -> List[VAny](3, 1, 2),
+      in(1, 2, 3) -> List[VAny](2, 1, 3),
       in(1) -> List[VAny](1),
     )
   }
