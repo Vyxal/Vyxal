725e7eacedf8a8c139c494d2d1b3d55e22d0bdba
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-12-12 16:03:51 +1100

    I have to do it all over again

--- shared/test/src/vyxal/ElementTests.scala
+++ shared/test/src/vyxal/ElementTests.scala
@@ -264,7 +264,7 @@ class ElementTests extends VyxalTests:
     testStackLike("Ɠ")(
       in(VList(1, 2, 3)) -> List[VAny](3, VList(1, 2, 3)),
       in(VList(1, 2, 3), VList(4, 5, 6)) ->
-        List[VAny](6, VList(1, 2, 3), VList(4, 5, 6)),
+        List[VAny](6, VList(4, 5, 6), VList(1, 2, 3)),
     )
   }
 
@@ -272,7 +272,7 @@ class ElementTests extends VyxalTests:
     testStackLike("ɠ")(
       in(VList(1, 2, 3)) -> List[VAny](1, VList(1, 2, 3)),
       in(VList(1, 2, 3), VList(4, 5, 6)) ->
-        List[VAny](1, VList(1, 2, 3), VList(4, 5, 6)),
+        List[VAny](1, VList(4, 5, 6), VList(1, 2, 3)),
     )
   }
 
