739506cef85b53aabfff9092ee9c707f4b3764f1
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-12-15 22:53:01 +1100

    Update ListHelpers.scala

--- shared/src/vyxal/ListHelpers.scala
+++ shared/src/vyxal/ListHelpers.scala
@@ -1067,7 +1067,7 @@ object ListHelpers:
     VList.from(temp)
 
   def uniqueBy(iterable: VList, fn: VFun)(using Context): VList =
-    val seen = mutable.ArrayBuffer.empty[VAny]
+    val seen = mut.ArrayBuffer.empty[VAny]
     VList.from(
       iterable.filter { elem =>
         val res = fn.execute(elem, 0, List(elem))
