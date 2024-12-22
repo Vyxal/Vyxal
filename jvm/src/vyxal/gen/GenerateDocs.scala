58d33a629615275fd1fc39e58ebecef87aba595b
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-12-22 13:37:41 +1100

    try new method

--- jvm/src/vyxal/gen/GenerateDocs.scala
+++ jvm/src/vyxal/gen/GenerateDocs.scala
@@ -13,39 +13,55 @@ import vyxal.SyntaxInfo
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 
+import os.copy.over
+
 @main def generateDocs(
     elementsFile: String,
     trigraphsFile: String,
     tableFile: String,
 ) =
   Files.write(
-    Paths.get(elementsFile),
-    elementsMd(ElementInformation.elements).getBytes(StandardCharsets.UTF_8),
+    Paths.get(tableFile),
+    genTable().getBytes(StandardCharsets.UTF_8),
   )
 
-def elementsMd(elements: Seq[Element]): String =
-  val tableHeader =
-    "| Symbol | Keywords | Arity | Vectorises | Peeks | Overloads |\n|---|---|---|---|---|---|"
+def genTable(): String =
+  val HEADER_ROW = "| Symbol | Arity | Vectorises | Peeks | Overloads |" +
+    "\n|--------|-------|------------|-------|-----------|"
 
-  val tableRows = elements
-    .map { element =>
-      val symbol = element.symbol
-      val keywords = element.keywords.mkString(", ")
-      val arity = element.arity
-      val vectorises = if element.options.vectorises then "Yes" else "No"
-      val peeks = if element.options.peeks then "Yes" else "No"
-      val overloads =
-        element.overloads.map(overloadToMarkdown).mkString("<br/>")
+  val elementMap = ElementInformation.elements
 
-      s"| $symbol | $keywords | $arity | $vectorises | $peeks | $overloads |"
+  val lines = elementMap.map { elem =>
+    val symbol = elem.symbol
+    val arity = elem.arity
+    val vectorises = elem.options.vectorises
+    val peeks = elem.options.peeks
+    val overloads = elem.overloads.map(overloadToString)
+    val overloadsFlat = overloads.foldLeft(Seq.empty[String]) {
+      case (acc, s: String) => acc :+ s
+      case (acc, s: Seq[String]) => acc ++ s
     }
-    .mkString("\n")
 
-  s"$tableHeader\n$tableRows"
-end elementsMd
+    s"| $symbol | $arity | $vectorises | $peeks | ${overloadsFlat.mkString("</br>")} |"
+  }
 
-private def overloadToMarkdown(overload: Overload): String =
-  val args = overload.args.mkString(", ")
-  val typeSwitchable =
-    if overload.typeSwitchable then " (Type-Switchable)" else ""
-  s"**${overload.name}**($args): ${overload.description}$typeSwitchable"
+  (HEADER_ROW +: lines).mkString("\n")
+end genTable
+
+private def overloadToString(overload: Overload): Seq[String] | String =
+  val description = overload.description
+  if overload.typeSwitchable then
+    // Extract all type switch templates
+    val FIELD_REGEX = """{((?:\\[{}|\\\\]|[^{}\\])*)}""".r
+    val fields = FIELD_REGEX.findAllMatchIn(description).map(_.group(1)).toSeq
+    val components = FIELD_REGEX.split(description).toSeq
+    val overloads = fields.map { field =>
+      field.split("(?<!\\)\\|").toSeq.permutations.toSeq
+    }
+    overloads.map { arg =>
+      val description =
+        components.zip(arg).map { case (c, a) => c + a }.mkString
+      s"${overload.name}: $description"
+    }
+  else s"${overload.name}: ${overload.description}"
+end overloadToString
