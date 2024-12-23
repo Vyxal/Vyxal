97ee71952ccaf1cd2260449164f6d2130d267b68
lyxal <36217120+Lyxal@users.noreply.github.com>
2024-12-23 23:38:21 +1100

    Update NewElements.scala

--- shared/src/vyxal/elements/NewElements.scala
+++ shared/src/vyxal/elements/NewElements.scala
@@ -1202,7 +1202,27 @@ object NewElements:
                 throw UnsupportedOverloadException("κ", "String | Function")
           case _ => throw UnsupportedOverloadException("κ", "String | Function")
       },
-    "⬱" -> direct(Monad) {},
+    "⬱" ->
+      direct(Monad) {
+        val index = pop()
+        index match
+          case simpleOuter: VNum =>
+            val ctx = summon[Context]
+            val parentCtx = ctx.getParentCtx.getOrElse(ctx)
+            val value = parentCtx.getStack(index)
+            push(value)
+          case coordinates: Seq[VNum] =>
+            if coordinates.length != 2 then
+              throw InvalidListOverloadException("⬱", coordinates, "2")
+            val ctx = summon[Context]
+            var parentCtx = ctx
+            for _ <- 0 until coordinates.head.toInt do
+              parentCtx = parentCtx.getParentCtx.getOrElse(parentCtx)
+            val value = parentCtx.getStack(coordinates(1))
+            push(value)
+          case _ => throw UnsupportedOverloadException("⬱", "Non-number")
+        end match
+      },
   )
 
   // Subject to being added as overloads onto things in elements
