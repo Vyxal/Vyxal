69d1aa2ab554e13757183b00ed864fd5c0075034
lyxal <36217120+Lyxal@users.noreply.github.com>
2025-01-04 15:31:34 +1100

    It does. That's a bit of a dilemma

--- shared/test/src/vyxal/VyxalTests.scala
+++ shared/test/src/vyxal/VyxalTests.scala
@@ -142,7 +142,7 @@ object VyxalTests:
         Settings(endPrintMode = EndPrintMode.None),
       )
     )
-    // globals.inputs = Inputs(inputs)
+    globals.inputs = Inputs(inputs)
     Context(
       inputs = inputs,
       testMode = true,
