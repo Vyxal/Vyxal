fc016d74bcb66358399eebffe6d2288e876c2c94
lyxal <36217120+lyxal@users.noreply.github.com>
2025-01-14 08:59:26 +1100

    Remove uppercase big lambda

--- shared/src/vyxal/parsing/SBCSLexer.scala
+++ shared/src/vyxal/parsing/SBCSLexer.scala
@@ -14,7 +14,7 @@ class SBCSLexer extends LexerCommon:
   private val DIGRAPH_CHARS = "∆øÞk"
   private val HASH_DIGRAPH_REGEX =
     """#[^\[\]$!=#>@{:]""" // Matches # followed by any character that doesn't start a trigraph
-  private val STRUCTURE_OPENERS = "[({ṆḌƛΛξ⍾ʎµ⟨⎊⎄"
+  private val STRUCTURE_OPENERS = "[({ṆḌƛξ⍾ʎµ⟨⎊⎄"
   private val IF_ELSE_OPENER = "#{"
   private val RECORD_OPENER = "#::R"
   private val EXTENSION_OPENER = "#::+"
