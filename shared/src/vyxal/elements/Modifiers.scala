98476bef451af8a13baf3186fc6d19016086ab92
lyxal <36217120+Lyxal@users.noreply.github.com>
2025-01-15 10:16:27 +1100

    Update Modifiers.scala

--- shared/src/vyxal/elements/Modifiers.scala
+++ shared/src/vyxal/elements/Modifiers.scala
@@ -86,7 +86,7 @@ object Monadic
   override def fill(symbol: String)(fn: PartialMonadicModifier) =
     arg =>
       if fn.isDefinedAt(arg) then fn(arg)
-      else throw UnimplementedModifierOverloadException(symbol, arg)
+      else throw UnimplementedModifierOverloadException(symbol, Seq(arg))
 
 object Dyadic extends ModifierHelpers[PartialDyadicModifier, DyadicModifier](2):
   override def toDirectFn(impl: DyadicModifier) =
@@ -99,7 +99,7 @@ object Dyadic extends ModifierHelpers[PartialDyadicModifier, DyadicModifier](2):
     (ast1, ast2) =>
       val args = (ast1, ast2)
       if fn.isDefinedAt(args) then fn(args)
-      else throw UnimplementedModifierOverloadException(symbol, arg)
+      else throw UnimplementedModifierOverloadException(symbol, Seq(ast1, ast2))
 
 object Triadic
     extends ModifierHelpers[PartialTriadicModifier, TriadicModifier](3):
@@ -115,7 +115,12 @@ object Triadic
     (ast1, ast2, ast3) =>
       val args = (ast1, ast2, ast3)
       if fn.isDefinedAt(args) then fn(args)
-      else throw UnimplementedModifierOverloadException(symbol, arg)
+      else
+        throw UnimplementedModifierOverloadException(
+          symbol,
+          Seq(ast1, ast2, ast3),
+        )
+end Triadic
 
 object Tetradic
     extends ModifierHelpers[PartialTetradicModifier, TetradicModifier](4):
@@ -131,7 +136,12 @@ object Tetradic
     (ast1, ast2, ast3, ast4) =>
       val args = (ast1, ast2, ast3, ast4)
       if fn.isDefinedAt(args) then fn(args)
-      else throw UnimplementedModifierOverloadException(symbol, arg)
+      else
+        throw UnimplementedModifierOverloadException(
+          symbol,
+          Seq(ast1, ast2, ast3, ast4),
+        )
+end Tetradic
 
 object Modifiers:
   case class Modifier(arity: Int, from: DirectFn)
