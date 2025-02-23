c721a5592710665b43848cee81c3c622203d3e45
lyxal <36217120+lyxal@users.noreply.github.com>
2025-02-23 22:58:07 +1100

    Bump internal version to 3.7.0 (part 1)

--- shared/src/vyxal/Interpreter.scala
+++ shared/src/vyxal/Interpreter.scala
@@ -10,7 +10,7 @@ import scala.collection.mutable.ListBuffer
 import scala.collection.mutable as mut
 
 object Interpreter:
-  def version = "3.6.1"
+  def version = "3.7.0"
   def execute(code: String)(using ctx: Context): Unit =
 
     /** Attempt lexing */
