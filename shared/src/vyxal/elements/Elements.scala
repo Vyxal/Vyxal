fb5aeb5d4868137a0925858f25264490de807cac
lyxal <36217120+lyxal@users.noreply.github.com>
2025-03-20 12:18:22 +1100

    Make Ƶ worm with empty lists

--- shared/src/vyxal/elements/Elements.scala
+++ shared/src/vyxal/elements/Elements.scala
@@ -37,7 +37,8 @@ object Elements:
     "Ƶ" ->
       direct(Monad) {
         val a = pop().itr
-        push(a.init, a.last)
+        if a.isEmpty then push(VList(Seq.Empty), 0)
+        else push(a.init, a.last)
       },
     "⊞" ->
       direct(Monad) {
