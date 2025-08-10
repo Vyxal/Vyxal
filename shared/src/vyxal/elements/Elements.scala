bc118d490913a47048f308a18795e534c7fa9452
pacmanboss256 <pacmanboss972@gmail.com>
2025-08-10 14:50:19 -0500

    fix spacing remove comment

--- shared/src/vyxal/elements/Elements.scala
+++ shared/src/vyxal/elements/Elements.scala
@@ -1017,11 +1017,7 @@ object Elements:
         while !predicate(VNum(res)).toBool do res += 1
         res
     },
-    addPart(
-      "Ϣ",
-      Dyad,
-      false,
-    ) { // Wrap to length of string in progress
+    addPart("Ϣ", Dyad, false) {
       case (a: VList, b: VNum) => ListHelpers.wrapLength(a, b)
       case (a: VNum, b: VNum) =>
         if b <= 0 then Seq.empty
