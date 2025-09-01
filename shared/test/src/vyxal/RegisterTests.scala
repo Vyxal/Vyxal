46f8432a4832a919ce3052574830dd0c80ae507f
pacmanboss256 <pacmanboss972@gmail.com>
2025-09-01 15:57:23 -0400

    add vec push register

--- shared/test/src/vyxal/RegisterTests.scala
+++ shared/test/src/vyxal/RegisterTests.scala
@@ -20,6 +20,12 @@ class RegisterTests extends VyxalTests:
         """  "vyxal " "cool" "is "⎇ £ J ¥ J """ -> VStr("vyxal is cool")
 
       )
+      it("Should pop after vectorizing") {
+        given ctx: Context = Context(testMode = true)
+        Interpreter.execute("5ʁ Þ£")
+        assert(ctx.isStackEmpty)
+      }
+      
       it("Should push zero if empty") {
         given ctx: Context = Context(testMode = true)
         Interpreter.execute("5£")
@@ -33,16 +39,16 @@ class RegisterTests extends VyxalTests:
     describe("Stack Register Stuff") {
       testMulti(
         "5£ 6£ Þ¥ Þ¥;" -> vSeq(vSeq(5,6), 0),
-        "5ʁ ¨£ ¥ Þ¥;" -> vSeq(5, vSeq(0,1,2,3,4,5)),
-        "5ʁ ¨£ Þ^ Þ¥" -> vSeq(5,4,3,2,1,0),
-        "5ʁ ¨£ Þ_ Þ¥" -> VNum(0)
+        "5ʁ Þ£ ¥ Þ¥;" -> vSeq(5, vSeq(0,1,2,3,4,5)),
+        "5ʁ Þ£ Þ^ Þ¥" -> vSeq(5,4,3,2,1,0),
+        "5ʁ Þ£ Þ_ Þ¥" -> VNum(0)
       )
     }
     describe("Indexing") {
       testMulti(
-        "5ʁ ¨£ 1Þ⦷" -> VNum(1),
-        "5ʁ ¨£ 13Þ⦷" -> VNum(1),
-        "5ʁ ¨£ 1N Þ⦷" -> VNum(5),
+        "5ʁ Þ£ 1Þ⦷" -> VNum(1),
+        "5ʁ Þ£ 13Þ⦷" -> VNum(1),
+        "5ʁ Þ£ 1N Þ⦷" -> VNum(5),
       )
     }
     describe("Function behavior") {
@@ -55,22 +61,22 @@ class RegisterTests extends VyxalTests:
       }
       describe("Don't Pop Functions") {
         testMulti(
-          "5ʁ ¨£ λ2+}£  Þ¥" -> vSeq(0,1,2,3,4,5),
+          "5ʁ Þ£ λ2+}£  Þ¥" -> vSeq(0,1,2,3,4,5),
           "λ2+}£ λ2+}£  Þ¥" -> Seq.empty
         )
       }
       describe("Apply to specific indexes") {
         testMulti(
-          "5ʁ ¨£ 2 λT} ÞϾ Þ¥" -> vSeq(0,1,6,3,4,5),
-          "5ʁ ¨£ λT} 2 ÞϾ Þ¥" -> vSeq(0,1,6,3,4,5),
-          "5ʁ ¨£ λT} 1 2; ÞϾ Þ¥" -> vSeq(0,3,6,3,4,5),
-          "5ʁ ¨£ 1 2; λT} ÞϾ Þ¥" -> vSeq(0,3,6,3,4,5),
+          "5ʁ Þ£ 2 λT} ÞϾ Þ¥" -> vSeq(0,1,6,3,4,5),
+          "5ʁ Þ£ λT} 2 ÞϾ Þ¥" -> vSeq(0,1,6,3,4,5),
+          "5ʁ Þ£ λT} 1 2; ÞϾ Þ¥" -> vSeq(0,3,6,3,4,5),
+          "5ʁ Þ£ 1 2; λT} ÞϾ Þ¥" -> vSeq(0,3,6,3,4,5),
         )
       }
       describe("Mapping") {
         testMulti(
-          "5ʁ ¨£ λ2+}⎘Þ¥" -> vSeq(2,3,4,5,6,7),
-          "5ʁ ¨£ λ2}⎘Þ¥" -> vSeq(2,2,2,2,2,2)
+          "5ʁ Þ£ λ2+}⎘Þ¥" -> vSeq(2,3,4,5,6,7),
+          "5ʁ Þ£ λ2}⎘Þ¥" -> vSeq(2,2,2,2,2,2)
         )
       }
       describe("Should apply functions after popping them") {
@@ -81,7 +87,7 @@ class RegisterTests extends VyxalTests:
       }
       
       it("Global context") {
-        testCode("5ʁ ¨£ 5 11R Þ^ƛ`}", vSeq(0,1,2,3,4,5))
+        testCode("5ʁ Þ£ 5 11R Þ^ƛ`}", vSeq(0,1,2,3,4,5))
       }
     }
 end RegisterTests
