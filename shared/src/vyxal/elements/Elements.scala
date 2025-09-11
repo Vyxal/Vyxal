9156dd2d357ce49ae3541211314a1b532d377a0e
pacmanboss256 <pacmanboss256@users.noreply.github.com>
2025-09-11 22:58:24 +0000

    Scalafix changes

--- shared/src/vyxal/elements/Elements.scala
+++ shared/src/vyxal/elements/Elements.scala
@@ -1985,19 +1985,19 @@ object Elements:
     },
     "Þ„" ->
       direct(Monad) {
-      val top = pop()
-      top match
-        case VList(lst) => push(ListHelpers.fromDiagonals(lst))
-        case n: VNum =>
-          val under = pop()
-          under match
-            case VList(lst) => push(ListHelpers.fromDiagonals(lst, Some(n)))
-            case _ => throw UnimplementedOverloadException("Þ„", List(under, n))
-        case _ => throw UnimplementedOverloadException("Þ„", List(top))
-    },
+        val top = pop()
+        top match
+          case VList(lst) => push(ListHelpers.fromDiagonals(lst))
+          case n: VNum =>
+            val under = pop()
+            under match
+              case VList(lst) => push(ListHelpers.fromDiagonals(lst, Some(n)))
+              case _ =>
+                throw UnimplementedOverloadException("Þ„", List(under, n))
+          case _ => throw UnimplementedOverloadException("Þ„", List(top))
+      },
     addPart("Þ”", Monad, false) {
-      case VList(lst) => 
-        ListHelpers.fromAntiDiagonals(lst)
+      case VList(lst) => ListHelpers.fromAntiDiagonals(lst)
     },
     "Þ⁰" ->
       fullToImpl(
