f8c4d31ba476366ed2dfc7a9fa125153233b9460
themoonisacheese <themoonisacheese@users.noreply.github.com>
2025-11-28 13:32:27 +0000

    Scalafix changes

--- shared/src/vyxal/elements/Elements.scala
+++ shared/src/vyxal/elements/Elements.scala
@@ -1241,8 +1241,16 @@ object Elements:
       case (a: VNum, b: VNum) =>
         if b == VNum(0) then NumberHelpers.round(a)
         else if b.toInt <= 9 then a - (a % (10 ** -(b.toInt)))
-          else if a == VNum(0) then a else
-            NumberHelpers.round((a * (10**b.toInt))).toString().patch(NumberHelpers.round(NumberHelpers.log(a,10) + 0.5).toInt, ".", 0) //fallback to return the string representation if the precision is too high
+        else if a == VNum(0) then a
+        else
+          NumberHelpers
+            .round((a * (10 ** b.toInt)))
+            .toString()
+            .patch(
+              NumberHelpers.round(NumberHelpers.log(a, 10) + 0.5).toInt,
+              ".",
+              0,
+            ) // fallback to return the string representation if the precision is too high
       case (VStr(a), VStr(b)) => StringHelpers.r(b).matches(a)
     },
     addPart("℗", Monad, true) {
