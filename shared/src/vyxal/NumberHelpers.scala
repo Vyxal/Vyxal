23ae63b6ff7bb9fbe0220c95c616f77746a71985
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2026-07-02 22:13:48 +0100

    Update NumberHelpers.scala

--- shared/src/vyxal/NumberHelpers.scala
+++ shared/src/vyxal/NumberHelpers.scala
@@ -455,10 +455,10 @@ object NumberHelpers:
     ((x + y) * (x + y + 1) / 2) + y
 
   def cantorUnpair(n: VNum)(using Context): Tuple =
-    val w = ((scala.math.sqrt(8 * n.underlying.real + 1) - 1) / 2).floor.toInt
+    val w = (((8 * n.underlying.real + 1).sqrt - 1) / 2).floor.toInt
     val t = ((w * w + w) / 2).toInt
     val y = n - t
     val x = w - y
-    (VNum(x), VNum(y))
+    (x, y)
 
 end NumberHelpers
