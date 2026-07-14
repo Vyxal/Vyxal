4b60416a89626f6137a04fff05d09ee79d2d476f
PlaceReporter99 <PlaceReporter99@users.noreply.github.com>
2026-07-14 07:47:12 +0000

    Scalafix changes

--- shared/src/vyxal/StringHelpers.scala
+++ shared/src/vyxal/StringHelpers.scala
@@ -558,9 +558,9 @@ object StringHelpers:
         ).grouped(8)
           .map((x: Array[Int]) =>
             VNum(
-              x.zipWithIndex
-                .map { case (item, index) => item << (7 - index) }
-                .sum
+              x.zipWithIndex.map {
+                case (item, index) => item << (7 - index)
+              }.sum
             )
           )
           .toArray
