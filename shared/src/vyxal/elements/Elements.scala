3ee9299242a9008c174c8bd06fe9cbb4b1f08a82
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2026-07-14 12:50:26 +0100

    Add breathing room for the code
    Maybe it wasn't compiling due to hypoxia

--- shared/src/vyxal/elements/Elements.scala
+++ shared/src/vyxal/elements/Elements.scala
@@ -152,8 +152,16 @@ object Elements:
       case VListOf[VNum](a) => if (a.itr.forall(case x: VNum => x.toInt < 256 && x.toInt >= 0 && x.isNatural case _ => false)) then VList(StringHelpers.hashBytes(a)) else throw new Exceptions.BadLHSException("ø⑦", a)
     },
     addPart("øH", Monad, true) {
-      case VStr(a) => if (a.toString.matches("^[0123456789abcdefABCDEF]*$")) then VList(a.toString.grouped(2).map((x: String) => VNum(Integer.parseInt(x, 16)))) else throw new Exceptions.BadLHSException("øH", a)
-      case VListOf[VNum](a) => if (a.itr.forall(case x: VNum => x.toInt < 256 && x.toInt >= 0 && x.isNatural case _ => false)) then VStr(a.map(case x: VNum => String.format("%02x", x)).mkString) else throw new Exceptions.BadLHSException("øH", a)
+      case VStr(a) =>
+        if (a.toString.matches("^[0123456789abcdefABCDEF]*$")) then 
+          VList(a.toString.grouped(2).map((x: String) => VNum(Integer.parseInt(x, 16))))
+        else 
+          throw new Exceptions.BadLHSException("øH", a)
+      case VListOf[VNum](a) =>
+        if (a.itr.forall(case x: VNum => x.toInt < 256 && x.toInt >= 0 && x.isNatural case _ => false)) then
+          VStr(a.map(case x: VNum => String.format("%02x", x)).mkString)
+        else
+          throw new Exceptions.BadLHSException("øH", a)
     }
     "%" -> fullToImpl(Dyad, MiscHelpers.modulo),
     addPart("&", Dyad, false) {
