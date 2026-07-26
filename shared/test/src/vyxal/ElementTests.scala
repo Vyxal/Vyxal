be41efe4b769f8903b6dfcb5c8d547ef589154d0
PlaceReporter99 <PlaceReporter99@users.noreply.github.com>
2026-07-26 13:14:16 +0000

    Scalafix changes

--- shared/test/src/vyxal/ElementTests.scala
+++ shared/test/src/vyxal/ElementTests.scala
@@ -707,14 +707,22 @@ class ElementTests extends VyxalTests:
     }
     it("unpausing and pausing should be idempotent, but toggling shouldn't") {
       def checkTimer(paused: Int, obj: VAny): Unit =
-        val (_, timeElapsed) = obj.asInstanceOf[VObject].fields("timeElapsed").asInstanceOf[VDuration]
-        val (_, timeRemaining) = obj.asInstanceOf[VObject].fields("timeRemaining").asInstanceOf[VDuration]
-        val (_, isPaused) = obj.asInstanceOf[VObject].fields("isPaused").asInstanceOf[VDuration]
+        val (_, timeElapsed) = obj
+          .asInstanceOf[VObject]
+          .fields("timeElapsed")
+          .asInstanceOf[VDuration]
+        val (_, timeRemaining) = obj
+          .asInstanceOf[VObject]
+          .fields("timeRemaining")
+          .asInstanceOf[VDuration]
+        val (_, isPaused) =
+          obj.asInstanceOf[VObject].fields("isPaused").asInstanceOf[VDuration]
         assert(
           timeElapsed.toMillis.toInt >= 0 && 5 >= timeElapsed.toMillis.toInt &&
             isPaused == VNum(paused) &&
             (timeElapsed + timeRemaining).toMillis.toInt == 1000
         )
+      end checkTimer
       given ctx: Context = Context(testMode = true)
       ctx.push(1000)
       Interpreter.execute(AST.Command("#O"))
