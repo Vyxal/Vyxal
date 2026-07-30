365fe05219e343d59bc702b7785faa8177a83f08
PlaceReporter99 <PlaceReporter99@users.noreply.github.com>
2026-07-30 17:35:00 +0000

    Scalafix changes

--- shared/test/src/vyxal/ElementTests.scala
+++ shared/test/src/vyxal/ElementTests.scala
@@ -846,7 +846,7 @@ class ElementTests extends VyxalTests:
 
       val func: VFun = ctx.pop().asInstanceOf[VFun]
 
-      assertResult(VNum(21))(func.apply(VNum(3), VNum(7))) 
+      assertResult(VNum(21))(func.apply(VNum(3), VNum(7)))
       assertResult(VNum(2))(func.apply(VStr("monday"), VStr("monkey")))
       assertResult(VList(Seq(VType(classOf[VNum]), VType(classOf[VNum]))))(
         func.apply(VType(classOf[VNum]), VType(classOf[VNum]))
